4dcf696b7e65002112aa607bd5ecc7452e1570ef
Mark Gwilliam <mark.gwilliam@valtech.co.uk>
2015-03-02 13:18:24 +0000

    comments for 1.7

--- src/SFA.Apprenticeships.Application.ApplicationUpdate/Strategies/ApplicationStatusChangedStrategy.cs
+++ src/SFA.Apprenticeships.Application.ApplicationUpdate/Strategies/ApplicationStatusChangedStrategy.cs
@@ -6,7 +6,9 @@
     {
         public void Send(ApplicationStatusSummary applicationStatusSummary)
         {
-            //todo: 1.7: publish communication message for later processing. details TBC - may be removed...
+            //todo: 1.7: publish communication message for later processing. 
+            //initially only interested in status changes of submitted applications (i.e. becoming successful or unsuccessful)
+            //details TBC - may be removed and replaced with queue message...
         }
     }
 }
