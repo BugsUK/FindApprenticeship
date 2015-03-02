fa40dde149c42ae29170456d55b9eefba596655c
Mark Gwilliam <mark.gwilliam@valtech.co.uk>
2015-03-02 13:35:50 +0000

    placeholder entity for application status notifications (US341)

---
+++ src/SFA.Apprenticeships.Domain.Entities/Communication/UpdatedApprenticeshipApplication.cs
@@ -0,0 +1,28 @@
+﻿namespace SFA.Apprenticeships.Domain.Entities.Communication
+{
+    using System;
+    using Applications;
+
+    public class UpdatedApprenticeshipApplication : BaseEntity
+    {
+        public Guid CandidateId { get; set; }
+
+        public Guid ApplicationId { get; set; }
+        
+        public int VacancyId { get; set; }
+
+        public string Title { get; set; }
+
+        public string EmployerName { get; set; }
+
+        public ApplicationStatuses Status { get; set; }
+
+        public string UnsuccessfulReason { get; set; }
+
+        public DateTime DateApplied { get; set; }
+
+        public Guid? BatchId { get; set; }
+
+        public DateTime? SentDateTime { get; set; }
+    }
+}
