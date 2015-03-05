9aa7b29e25b75d9589a508535384b70f904ee087
Krister Bone <krister.bone@valtech.co.uk>
2015-03-05 11:46:59 +0000

    US65: Test saved apps converted to drafts

--- src/SFA.Apprenticeships.Web.Candidate/Providers/ApprenticeshipApplicationProvider.cs
+++ src/SFA.Apprenticeships.Web.Candidate/Providers/ApprenticeshipApplicationProvider.cs
@@ -146,14 +146,6 @@
             }
         }
 
-        //public ApprenticeshipApplicationViewModel CreateDraftApplicationFromSavedVacancy(Guid candidateId, int vacancyId)
-        //{
-        //    _logger.Debug("Calling ApprenticeshipApplicationProvider to get the Application View Model for candidate ID: {0}, vacancy ID: {1}.", candidateId, vacancyId);
-        //    var applicationDetail = _candidateService.CreateDraftFromSavedVacancy(candidateId, vacancyId);
-        //    var applicationViewModel = _mapper.Map<ApprenticeshipApplicationDetail, ApprenticeshipApplicationViewModel>(applicationDetail);
-        //    return applicationViewModel;
-        //}
-
         public ApprenticeshipApplicationViewModel PatchApplicationViewModel(Guid candidateId,
             ApprenticeshipApplicationViewModel savedModel, ApprenticeshipApplicationViewModel submittedModel)
         {
