41a40b11ad8578ea3dc1011cf62a3660ca254f7a
David Winchurch <david.winchurch@iterativeit.com>
2015-03-06 15:04:55 +0000

    Adding todo

--- src/SFA.Apprenticeships.Web.Candidate/Controllers/ApprenticeshipApplicationController.cs
+++ src/SFA.Apprenticeships.Web.Candidate/Controllers/ApprenticeshipApplicationController.cs
@@ -126,6 +126,7 @@
         {
             return await Task.Run<ActionResult>(() =>
             {
+                //TODO: Remove once the cause of the null candidate objects is fixed
                 if (model.Candidate == null)
                 {
                     var requestDebugString = Request.ToDebugString();
@@ -169,6 +170,7 @@
         {
             return await Task.Run(() =>
             {
+                //TODO: Remove once the cause of the null candidate objects is fixed
                 if (model.Candidate == null)
                 {
                     var requestDebugString = Request.ToDebugString();
