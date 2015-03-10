91ed4cfcb844ad39b5d85b4bf902f8c49101fb09
Mark Gwilliam <mark.gwilliam@valtech.co.uk>
2015-03-10 10:20:34 +0000

    1.8 comments

--- src/SFA.Apprenticeships.Application.Vacancies/ISavedSearchProcessor.cs
+++ src/SFA.Apprenticeships.Application.Vacancies/ISavedSearchProcessor.cs
@@ -5,8 +5,12 @@
 
     public interface ISavedSearchProcessor
     {
+        //todo: 1.8: queue CandidateSavedSearches to rabbit for *active* candidates with comm prefs enabled and saved searches (that have alerts enabled)
         void QueueSavedSearchCandidates();
 
+        //todo: 1.8: retrieve top 5 most recent search results for each saved search (use IVacancySearchProvider to execute)
+        // write results to SavedSearchAlerts repo, upsert based on candidate+savedsearch+date (assuming for now only runs once per day)
+        // note: should not write if no new results (check datetime of SavedSearch)
         void ProcessSavedSearchCandidate(CandidateSavedSearches candidateSavedSearches);
     }
 }
--- src/SFA.Apprenticeships.Domain.Entities/Candidates/SavedSearch.cs
+++ src/SFA.Apprenticeships.Domain.Entities/Candidates/SavedSearch.cs
@@ -5,6 +5,7 @@
     public class SavedSearch : BaseEntity
     {
         //todo: 1.8: saved search definition (search parameters, alerts options, etc.)
+        //note: need to store datetime last executed to ensure alerts only triggered if there are new results
         //note: not all parameters used to *execute* a search are saved as the definition of a saved search (e.g. ordering, page number, page size)
         // location, distance, level, [keyword | category, subcategories]
         public string Name { get; set; }
