7c3133fcb3a18562c342f952c19db8ec74bc1a6b
Mark Gwilliam <mark.gwilliam@valtech.co.uk>
2015-03-10 11:25:25 +0000

    1.8 comments and placeholders

--- src/SFA.Apprenticeships.Application.Vacancies/SavedSearchProcessor.cs
+++ src/SFA.Apprenticeships.Application.Vacancies/SavedSearchProcessor.cs
@@ -14,8 +14,8 @@
         {
             //todo: 1.8: retrieve top 5 most recent search results for each saved search (use IVacancySearchProvider to execute)
             // write results to SavedSearchAlerts repo, upsert based on candidate+savedsearch+date (assuming for now only runs once per day)
-            // note: should not write if no new results (check datetime of SavedSearch) since last *sent* search
-            // note: update SavedSearch.LastResultsHash to ensure alerts only triggered if there are new results
+            // note: should not write if no new results (check LastResultsHash if any) since last *sent* search
+            // note: update SavedSearch.LastResultsHash to ensure alerts are only triggered if there are new results
             // note: update SavedSearch.DateProcessed with datetime processed (if changes)
 
             //todo: once we have the vacancy posted date (March 2015) we may store this instead of the processed date
--- src/SFA.Apprenticeships.Domain.Entities/Communication/SavedSearchAlert.cs
+++ src/SFA.Apprenticeships.Domain.Entities/Communication/SavedSearchAlert.cs
@@ -1,13 +1,17 @@
 ﻿namespace SFA.Apprenticeships.Domain.Entities.Communication
 {
     using System;
+    using System.Collections.Generic;
     using Candidates;
+    using Vacancies.Apprenticeships;
 
     public class SavedSearchAlert : BaseEntity
     {
         //todo: 1.8: properties for a saved search alert item (apprenticeships only)
         //todo: don't automap entity properties!
-        public SavedSearch Search { get; set; }
+        public SavedSearch Parameters { get; set; }
+
+        public IEnumerable<ApprenticeshipSummary> Results { get; set; }
 
         public Guid? BatchId { get; set; }
 
