1229a3ed51d3f2d9026288ee69f1d820899b17bb
Mark Gwilliam <mark.gwilliam@valtech.co.uk>
2015-03-10 09:29:36 +0000

    1.8 comments

--- src/SFA.Apprenticeships.Domain.Entities/Candidates/SavedSearch.cs
+++ src/SFA.Apprenticeships.Domain.Entities/Candidates/SavedSearch.cs
@@ -5,6 +5,8 @@
     public class SavedSearch : BaseEntity
     {
         //todo: 1.8: saved search definition (search parameters, alerts options, etc.)
+        //note: not all parameters used to *execute* a search are saved as the definition of a saved search (e.g. ordering, page number, page size)
+        // location, distance, level, [keyword | category, subcategories]
         public string Name { get; set; }
     }
 }
