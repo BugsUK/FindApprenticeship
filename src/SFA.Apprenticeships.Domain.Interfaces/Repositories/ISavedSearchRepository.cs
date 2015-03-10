7a8a2c88344984d81b5f271f4a0b5d7564699360
Mark Gwilliam <mark.gwilliam@valtech.co.uk>
2015-03-10 05:01:45 +0000

    more placeholders and comments for 1.8

---
+++ src/SFA.Apprenticeships.Domain.Interfaces/Repositories/ISavedSearchRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace SFA.Apprenticeships.Domain.Interfaces.Repositories
+{
+    using System;
+    using Entities.Candidates;
+
+    public interface ISavedSearchReadRepository : IReadRepository<SavedSearch> {}
+
+    public interface ISavedSearchWriteRepository : IWriteRepository<SavedSearch> {}
+}
---
+++ src/SFA.Apprenticeships.Infrastructure.Repositories.Candidates/SavedSearchRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace SFA.Apprenticeships.Infrastructure.Repositories.Candidates
+{
+    using System;
+
+    public class SavedSearchRepository
+    {
+        //todo: 1.8: implement saved search repo
+    }
+}
