f841dbce646f52a7747abf127bb03cc6d50b910d
alangorton <alan.gorton@valtech.co.uk>
2015-03-17 12:55:12 +0000

    US638: implement missing SavedSearchAlertRepository members.

--- src/SFA.Apprenticeships.Infrastructure.Repositories.Communication/SavedSearchAlertRepository.cs
+++ src/SFA.Apprenticeships.Infrastructure.Repositories.Communication/SavedSearchAlertRepository.cs
@@ -10,15 +10,17 @@
     using Domain.Interfaces.Mapping;
     using Domain.Interfaces.Repositories;
     using Entities;
+    using MongoDB.Bson;
+    using MongoDB.Driver.Builders;
     using MongoDB.Driver.Linq;
 
-    //todo: 1.8: comms repo for saved search alerts
     public class SavedSearchAlertRepository : CommunicationRepository<SavedSearchAlert>, ISavedSearchAlertRepository
     {
         private readonly IMapper _mapper;
         private readonly ILogService _logger;
 
-        public SavedSearchAlertRepository(IConfigurationManager configurationManager, IMapper mapper, ILogService logger) : base(configurationManager, "savedsearchalerts")
+        public SavedSearchAlertRepository(IConfigurationManager configurationManager, IMapper mapper, ILogService logger)
+            : base(configurationManager, "savedsearchalerts")
         {
             _mapper = mapper;
             _logger = logger;
@@ -26,16 +28,19 @@
 
         public SavedSearchAlert GetUnsentSavedSearchAlert(SavedSearch savedSearch)
         {
-            _logger.Debug("Calling repository to get unsent saved search alert for saved search Id={0}", savedSearch.EntityId);
+            _logger.Debug("Calling repository to get unsent saved search alert for saved search Id={0}",
+                savedSearch.EntityId);
 
-            var mongoAlert = Collection.AsQueryable<MongoSavedSearchAlert>().SingleOrDefault(a => a.BatchId == null && a.Parameters.EntityId == savedSearch.EntityId);
+            var mongoAlert =
+                Collection.AsQueryable<MongoSavedSearchAlert>()
+                    .SingleOrDefault(a => a.BatchId == null && a.Parameters.EntityId == savedSearch.EntityId);
 
             if (mongoAlert == null)
             {
                 _logger.Debug("Did not find unsent saved search alert for saved search Id={0}", savedSearch.EntityId);
                 return null;
             }
-            
+
             _logger.Debug("Found unsent saved search alert for saved search Id={0}", savedSearch.EntityId);
             var alert = _mapper.Map<MongoSavedSearchAlert, SavedSearchAlert>(mongoAlert);
             return alert;
@@ -43,7 +48,9 @@
 
         public void Save(SavedSearchAlert savedSearchAlert)
         {
-            _logger.Debug("Calling repository to save saved search alert with Id={0} for saved search with Id={1} and CandidateId={2}", savedSearchAlert.EntityId, savedSearchAlert.Parameters.EntityId, savedSearchAlert.Parameters.CandidateId);
+            _logger.Debug(
+                "Calling repository to save saved search alert with Id={0} for saved search with Id={1} and CandidateId={2}",
+                savedSearchAlert.EntityId, savedSearchAlert.Parameters.EntityId, savedSearchAlert.Parameters.CandidateId);
 
             var mongoAlert = _mapper.Map<SavedSearchAlert, MongoSavedSearchAlert>(savedSearchAlert);
             UpdateEntityTimestamps(mongoAlert);
@@ -51,17 +58,32 @@
 
             Collection.Save(mongoAlert);
 
-            _logger.Debug("Saved saved search alert with Id={0} for saved search with Id={1} and CandidateId={2}", savedSearchAlert.EntityId, savedSearchAlert.Parameters.EntityId, savedSearchAlert.Parameters.CandidateId);
+            _logger.Debug("Saved saved search alert with Id={0} for saved search with Id={1} and CandidateId={2}",
+                savedSearchAlert.EntityId, savedSearchAlert.Parameters.EntityId, savedSearchAlert.Parameters.CandidateId);
         }
 
         public void Delete(SavedSearchAlert savedSearchAlert)
         {
-            throw new NotImplementedException();
+            _logger.Debug("Calling repository to delete saved search alert with Id={0}", savedSearchAlert.EntityId);
+
+            Collection.Remove(Query.EQ("_id", savedSearchAlert.EntityId));
+
+            _logger.Debug("Deleted saved search alert with Id={0}", savedSearchAlert.EntityId);
         }
 
         public Dictionary<Guid, List<SavedSearchAlert>> GetCandidatesSavedSearchAlerts()
         {
-            throw new NotImplementedException();
+            _logger.Debug("Calling repository to get all saved search alerts");
+
+            var mongoSavedSearchAlerts = Collection.FindAs<MongoSavedSearchAlert>(Query.EQ("BatchId", BsonNull.Value));
+            var savedSearchAlerts = _mapper.Map<IEnumerable<MongoSavedSearchAlert>, IEnumerable<SavedSearchAlert>>(mongoSavedSearchAlerts);
+            var candidatesSavedSearchAlerts = savedSearchAlerts
+                .GroupBy(x => x.Parameters.CandidateId)
+                .ToDictionary(grp => grp.Key, grp => grp.ToList());
+
+            _logger.Debug("Found saved search alerts for {0} candidates", candidatesSavedSearchAlerts.Count);
+
+            return candidatesSavedSearchAlerts;
         }
     }
 }
