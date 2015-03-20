c2288098463d87693b006af44cf05adbaaa48246
Mark Gwilliam <github@markgwilliam.com>
2015-03-20 10:31:13 +0000

    added placeholder IConfigurationService

---
+++ src/SFA.Apprenticeships.Domain.Interfaces/Configuration/IConfigurationService.cs
@@ -0,0 +1,9 @@
+﻿namespace SFA.Apprenticeships.Domain.Interfaces.Configuration
+{
+    using System;
+
+    public interface IConfigurationService
+    {
+        //todo: define simple interface for configuration (string based - no enums!!)
+    }
+}
