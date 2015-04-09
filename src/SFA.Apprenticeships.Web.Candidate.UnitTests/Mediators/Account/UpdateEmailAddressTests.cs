ebe2dd2cdff4ae32616ab48b1e9d8b19430c7456
Krister Bone <krister.bone@valtech.co.uk>
2015-04-09 09:14:22 +0100

    US711: Added mediator outline test files

---
+++ src/SFA.Apprenticeships.Web.Candidate.UnitTests/Mediators/Account/UpdateEmailAddressTests.cs
@@ -0,0 +1,9 @@
+﻿namespace SFA.Apprenticeships.Web.Candidate.UnitTests.Mediators.Account
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class UpdateEmailAddressTests
+    {
+    }
+}
---
+++ src/SFA.Apprenticeships.Web.Candidate.UnitTests/Mediators/Account/VerifyUpdatedEmailAddressTests.cs
@@ -0,0 +1,9 @@
+﻿namespace SFA.Apprenticeships.Web.Candidate.UnitTests.Mediators.Account
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    class VerifyUpdatedEmailAddressTests
+    {
+    }
+}
