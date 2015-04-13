77e0c2816d20481f6f5fa8fb5e97e651d308f610
charge-valtech <henry.charge@valtech.co.uk>
2015-04-13 09:19:14 +0100

    Remove fixed header on scroll

--- src/SFA.Apprenticeships.Web.Candidate/Content/_assets/js/nas/savedAndDraftCount.js
+++ src/SFA.Apprenticeships.Web.Candidate/Content/_assets/js/nas/savedAndDraftCount.js
@@ -18,9 +18,9 @@ function IncrementSavedAndDraftCount() {
     savedSearchTitle.text(currentCount + " Saved");
     savedSearchTitle.attr("saved-count", currentCount);
 
-    //if ($(window).scrollTop() > heightOfHeader) {
-    //    addFixedHeader();
-    //}
+    if ($(window).scrollTop() > heightOfHeader) {
+        addFixedHeader();
+    }
 }
 
 function DecrementSavedAndDraftCount() {
@@ -31,14 +31,14 @@ function DecrementSavedAndDraftCount() {
         savedSearchTitle.text("");
     } else {
         savedSearchTitle.text(currentCount + " Saved");
-        //if ($(window).scrollTop() > heightOfHeader) {
-        //    addFixedHeader();
-        //}
+        if ($(window).scrollTop() > heightOfHeader) {
+            addFixedHeader();
+        }
     }
     savedSearchTitle.attr("saved-count", currentCount);
 }
 
-//$(window).on('scroll', showFixedHeader);
+$(window).on('scroll', removeFixedHeader);
 
 function showFixedHeader() {
     var nowScrollTop = $(window).scrollTop();
