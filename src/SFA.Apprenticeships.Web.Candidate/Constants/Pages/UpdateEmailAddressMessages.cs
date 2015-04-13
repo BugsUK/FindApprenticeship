ed14faa90062f687ca73a9dca03b53296dad6c58
Krister Bone <krister.bone@valtech.co.uk>
2015-04-13 11:27:05 +0100

    US711: updated error messages (perhaps needs revisited to show messages collectively)

--- src/SFA.Apprenticeships.Web.Candidate/Constants/Pages/UpdateEmailAddressMessages.cs
+++ src/SFA.Apprenticeships.Web.Candidate/Constants/Pages/UpdateEmailAddressMessages.cs
@@ -1,12 +1,16 @@
 ﻿
 namespace SFA.Apprenticeships.Web.Candidate.Constants.Pages
 {
+    using FluentValidation.Results;
+
     public class UpdateEmailAddressMessages
     {
         public const string VerificationCodeSent = "A verification code has been sent to your new email address.";
         public const string UpdateEmailCodeResent = "A verification code has been resent to your new email address.";
         public const string UpdateEmailAddressError = "There’s been an error. Please try again.";
+        public const string UpdateEmailAddressAlreadyInUse = "The email address is not available.";
         public const string UpdatedEmailSuccess = "Your email address has been updated, please login using your new details.";
-        
+        public const string IncorrectVertificationCode = "The verification code entered is incorrect.";
+        public const string IncorrectPassword = "The password entered is incorrect.";
     }
 }
