c783aa683683cacaab5a6608698eae2f4b02c17a
alangorton <alan.gorton@valtech.co.uk>
2015-04-16 09:57:59 +0100

    Added one more SaveVacancy and DeleteSavedVacancy mediator test.

--- src/SFA.Apprenticeships.Web.Candidate.UnitTests/Mediators/ApprenticeshipApplication/SaveVacancyTests.cs
+++ src/SFA.Apprenticeships.Web.Candidate.UnitTests/Mediators/ApprenticeshipApplication/SaveVacancyTests.cs
@@ -45,6 +45,33 @@
             response.ViewModel.Should().Be(viewModel);
         }
 
+        [TestCase("1")]
+        [TestCase("0")]
+        [TestCase("X")]
+        public void ShouldSetSavedVacancyCountToZero(string savedAndDraftCount)
+        {
+            // Arrange.
+            var viewModel = new SavedVacancyViewModel();
+
+            ApprenticeshipApplicationProvider
+                .Setup(mock => mock.DeleteSavedVacancy(_testCandidateId, TestVacancyId))
+                .Returns(viewModel);
+
+            UserDataProvider
+                .Setup(mock => mock.Get(UserDataItemNames.SavedAndDraftCount))
+                .Returns(savedAndDraftCount);
+
+            // Act.
+            var response = Mediator.DeleteSavedVacancy(_testCandidateId, TestVacancyId);
+
+            // Assert.
+            UserDataProvider.Verify(mock => mock.Get(UserDataItemNames.SavedAndDraftCount), Times.Once);
+            UserDataProvider.Verify(mock => mock.Push(UserDataItemNames.SavedAndDraftCount, "0"), Times.Once);
+
+            response.AssertCode(ApprenticeshipApplicationMediatorCodes.DeleteSavedVacancy.Ok, true);
+            response.ViewModel.Should().Be(viewModel);
+        }
+
         [TestCase(1)]
         [TestCase(5)]
         public void ShouldIncrementSavedVacancyCount(int savedAndDraftCount)
