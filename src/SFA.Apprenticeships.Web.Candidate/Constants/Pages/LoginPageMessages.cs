196eca4d0d6cb7513447d30d552a81032fce6dce
alangorton <alan.gorton@valtech.co.uk>
2015-04-20 15:41:23 +0100

    Fix developer content for mobile and email verification prompts.

--- src/SFA.Apprenticeships.Web.Candidate/Constants/Pages/LoginPageMessages.cs
+++ src/SFA.Apprenticeships.Web.Candidate/Constants/Pages/LoginPageMessages.cs
@@ -4,7 +4,7 @@
     {
         public const string InvalidUsernameOrPasswordErrorText = "Please enter a valid email address or password";
         public const string LoginFailedErrorText = "There's been a problem signing in. Please try again.";
-        public const string MobileVerificationRequiredText = "Your mobile number has not yet been verified. You will not receive updates by text until you complete this process. <a href=\"{0}\">Click here to verify your number</a>";
-        public const string PendingUsernameVerificationRequiredText = "TODO: Your new email address has not yet been verified. You will be unable to sign in using your new email address until you complete this process. <a href=\"{0}\">Click here to verify your email address</a>";
+        public const string MobileVerificationRequiredText = "Your mobile number has not yet been verified. You won’t receive updates by text message until you <a href=\"{0}\">verify your number</a>.";
+        public const string PendingUsernameVerificationRequiredText = "Your new email address has not yet been verified. You’ll be unable to sign in using your new email address until you <a href=\"{0}\">verify your email address</a>.";
     }
 }
