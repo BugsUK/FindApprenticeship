d7b5dc8551d44c71aad571279c5bc41b828bccd6
Krister Bone <krister.bone@valtech.co.uk>
2015-04-29 14:54:49 +0100

    US779: Added basic view tests

--- src/SFA.Apprenticeships.Web.Candidate.UnitTests/Views/ApprenticeshipApplication/WhatHappensNextTests.cs
+++ src/SFA.Apprenticeships.Web.Candidate.UnitTests/Views/ApprenticeshipApplication/WhatHappensNextTests.cs
@@ -1,14 +1,79 @@
 ﻿namespace SFA.Apprenticeships.Web.Candidate.UnitTests.Views.ApprenticeshipApplication
 {
+    using System.Linq;
+    using Candidate.ViewModels.Applications;
+    using Candidate.ViewModels.VacancySearch;
+    using FluentAssertions;
     using NUnit.Framework;
+    using Ploeh.AutoFixture;
+    using RazorGenerator.Testing;
 
     [TestFixture]
     public class WhatHappensNextTests : ViewUnitTest
     {
         [Test]
-        public void TBC()
+        public void ShowsSearchReturnUrlWhenPResent()
         {
-            //TODO: Add UI tests for US779
+            //Arrange
+            const string returnUrl = "http://searchreturn";
+            var whatHappensNextViewModel = new WhatHappensNextApprenticeshipViewModel
+            {
+                SuggestedVacanciesSearchViewModel = new ApprenticeshipSearchViewModel()
+            };
+            var whatHappendNextView = new WhatHappensNextViewBuilder().Build();
+            whatHappendNextView.ViewBag.SearchReturnUrl = returnUrl;
+
+            //Act
+            var result = whatHappendNextView.RenderAsHtml(whatHappensNextViewModel);
+
+            //Assert
+            var searchLink = result.GetElementbyId("search-return-link");
+            searchLink.Should().NotBeNull();
+            searchLink.GetAttributeValue("href", returnUrl);
+        }
+
+        [Test]
+        public void ShowsFindApprenticeshipButtonIfNoSuggestedVacancies()
+        {
+            //Arrange
+            var whatHappensNextViewModel = new WhatHappensNextApprenticeshipViewModel
+            {
+                SuggestedVacanciesSearchViewModel = new ApprenticeshipSearchViewModel()
+            };
+            var whatHappendNextView = new WhatHappensNextViewBuilder().Build();
+
+            //Act
+            var result = whatHappendNextView.RenderAsHtml(whatHappensNextViewModel);
+
+            //Assert
+            var searchLink = result.GetElementbyId("find-apprenticeship-btn");
+            searchLink.Should().NotBeNull();
+            searchLink.InnerText.Should().Be("Find an apprenticeship");
+        }
+
+        [Test]
+        public void ShowsSuggestedApprenticeshipsAndSuggestedSearchUrl()
+        {
+            //Arrange            
+            var fixture = new Fixture();
+            var suggestedViewModels = fixture.CreateMany<SuggestedVacancyViewModel>().ToList();
+            var whatHappensNextViewModel = new WhatHappensNextApprenticeshipViewModel
+            {
+                SuggestedVacancies = suggestedViewModels,
+                SuggestedVacanciesSearchViewModel = new ApprenticeshipSearchViewModel() 
+            };
+
+            //Act
+            var whatHappendNextView = new WhatHappensNextViewBuilder().With(whatHappensNextViewModel).RenderAsHtml();
+            whatHappendNextView.Should().NotBeNull();
+
+            //Assert
+            for (int i = 0; i < 3; i++)
+            {
+                var suggestedVac = whatHappendNextView.GetElementbyId("suggested-vacancy-" + suggestedViewModels[i].VacancyId);
+                suggestedVac.InnerText.Should().Contain(suggestedViewModels[i].VacancyTitle);
+                suggestedVac.InnerText.Should().Contain(suggestedViewModels[i].Distance);
+            }
         }
     }
 }
---
+++ src/SFA.Apprenticeships.Web.Candidate.UnitTests/Views/ApprenticeshipApplication/WhatHappensNextViewBuilder.cs
@@ -0,0 +1,35 @@
+﻿namespace SFA.Apprenticeships.Web.Candidate.UnitTests.Views.ApprenticeshipApplication
+{
+    using Candidate.ViewModels.Applications;
+    using Candidate.Views.ApprenticeshipApplication;
+    using HtmlAgilityPack;
+    using RazorGenerator.Testing;
+
+    public class WhatHappensNextViewBuilder : ViewBuilderBase
+    {
+        private WhatHappensNextApprenticeshipViewModel _viewModel;
+        private WhatHappensNext _view;
+
+        public WhatHappensNextViewBuilder With(WhatHappensNextApprenticeshipViewModel viewModel)
+        {
+            _viewModel = viewModel;
+            return this;
+        }
+
+        public WhatHappensNext Build()
+        {
+            var view = new WhatHappensNext { ViewData = { Model = _viewModel } };
+            return view;
+        }
+
+        public HtmlDocument RenderAsHtml()
+        {
+            if (_view == null)
+            {
+                _view = Build();
+            }
+            var result = _view.RenderAsHtml(_viewModel);
+            return result;
+        } 
+    }
+}
