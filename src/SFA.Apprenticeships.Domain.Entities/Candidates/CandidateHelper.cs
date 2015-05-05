c2dea2aad0ba7c94fb12a922230b42f2ba13ad29
alangorton <alan.gorton@valtech.co.uk>
2015-05-05 16:58:12 +0100

    US733: refactor candidate helpers into a single class.

--- src/SFA.Apprenticeships.Domain.Entities/Candidates/CandidateHelper.cs
+++ src/SFA.Apprenticeships.Domain.Entities/Candidates/CandidateHelper.cs
@@ -1,5 +1,8 @@
 ﻿namespace SFA.Apprenticeships.Domain.Entities.Candidates
 {
+    using System.Collections.Generic;
+    using System.Linq;
+
     public static class CandidateHelper
     {
         public static bool MobileVerificationRequired(this Candidate candidate)
@@ -7,15 +10,15 @@
             var communicationPreferences = candidate.CommunicationPreferences;
 
             return !communicationPreferences.VerifiedMobile &&
-                   (communicationPreferences.IsAnyTextCommunicationEnabled());
+                   (IsAnyTextCommunicationEnabled(communicationPreferences));
         }
 
         public static bool AllowsCommunication(this Candidate candidate)
         {
             var communicationPreferences = candidate.CommunicationPreferences;
 
-            return communicationPreferences.IsAnyEmailCommunicationEnabled() ||
-                   (communicationPreferences.IsAnyTextCommunicationEnabled() && communicationPreferences.VerifiedMobile);
+            return IsAnyEmailCommunicationEnabled(communicationPreferences) ||
+                   (IsAnyTextCommunicationEnabled(communicationPreferences) && communicationPreferences.VerifiedMobile);
         }
 
         public static bool ShouldSendSavedSearchAlerts(this Candidate candidate)
@@ -26,5 +29,31 @@
             return savedSearchPreferences.EnableEmail ||
                    (savedSearchPreferences.EnableText && communicationPreferences.VerifiedMobile);
         }
+
+        #region Helpers
+
+        private static bool IsAnyEmailCommunicationEnabled(CommunicationPreferences communicationPreferences)
+        {
+            return IndividualCommunicationPreferences(communicationPreferences).Any(each => each.EnableEmail);
+        }
+
+        private static bool IsAnyTextCommunicationEnabled(CommunicationPreferences communicationPreferences)
+        {
+            return IndividualCommunicationPreferences(communicationPreferences).Any(each => each.EnableText);
+        }
+
+        private static IEnumerable<CommunicationPreference> IndividualCommunicationPreferences(
+            CommunicationPreferences communicationPreferences)
+        {
+            return new[]
+            {
+                communicationPreferences.ApplicationStatusChangePreferences,
+                communicationPreferences.ExpiringApplicationPreferences,
+                communicationPreferences.SavedSearchPreferences,
+                communicationPreferences.MarketingPreferences
+            };
+        }
+
+        #endregion
     }
 }
