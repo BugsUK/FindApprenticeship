b13153c74485cb2924b3087e688ca1814370936e
charge-valtech <henry.charge@valtech.co.uk>
2015-05-07 14:44:33 +0100

    Updated versioned CSS for auto save notification

--- src/SFA.Apprenticeships.Web.Candidate/Views/Shared/_ErrorLayout.generated.cs
+++ src/SFA.Apprenticeships.Web.Candidate/Views/Shared/_ErrorLayout.generated.cs
@@ -87,7 +87,7 @@ WriteLiteral(">\r\n    <!--[if gt IE 8]><!-->");
 
             
             #line 13 "..\..\Views\Shared\_ErrorLayout.cshtml"
-                     Write(Styles.Render(Url.CdnContent("css/main-1.10.0.2059.css", "~/Content/_assets")));
+                     Write(Styles.Render(Url.CdnContent("css/main-1.10.0.2071.css", "~/Content/_assets")));
 
             
             #line default
@@ -96,7 +96,7 @@ WriteLiteral("<!--<![endif]-->\r\n    <!--[if lte IE 8]>");
 
             
             #line 14 "..\..\Views\Shared\_ErrorLayout.cshtml"
-                 Write(Styles.Render(Url.CdnContent("css/main-ie8-1.10.0.2059.css", "~/Content/_assets")));
+                 Write(Styles.Render(Url.CdnContent("css/main-ie8-1.10.0.2071.css", "~/Content/_assets")));
 
             
             #line default
