3831fcff7cab2ba309142a5b06600bcf03b35286
Krister Bone <krister.bone@valtech.co.uk>
2015-05-18 11:36:41 +0100

    Fixing ToYear validation issue (in production) and updates to regular expression range test plus unit tests

--- src/SFA.Apprenticeships.Web.Common/Constants/YearRangeRegexGenerator.cs
+++ src/SFA.Apprenticeships.Web.Common/Constants/YearRangeRegexGenerator.cs
@@ -6,11 +6,17 @@
     {
         public static string GetRegex(string year)
         {
+            if (string.IsNullOrEmpty(year) || year.Length != 4)
+            {
+                //Will alway fail.
+                return "$^";
+            }
+
             var d = int.Parse(year[2].ToString(CultureInfo.InvariantCulture));
             var u = int.Parse(year[3].ToString(CultureInfo.InvariantCulture));
             var nextD = d + 1;
 
-            var regex = string.Format(@"19{0}[{1}-9]|19[{2}-9]\d|200\d", d, u, nextD);
+            var regex = string.Format(@"19{0}[{1}-9]|19[{2}-9]\d|20{0}[0-{1}]", d, u, nextD);
 
             for (var i = 1; i <= d; i++)
             {
