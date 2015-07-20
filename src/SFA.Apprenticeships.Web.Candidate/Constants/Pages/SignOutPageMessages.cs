7b6b9102e646b33fd6bce6ea88a83816c52d459c
David Winchurch <david.winchurch@iterativeit.com>
2015-07-20 15:30:08 +0100

    Defect: Feedback link in sign out page now opens in new window

--- src/SFA.Apprenticeships.Web.Candidate/Constants/Pages/SignOutPageMessages.cs
+++ src/SFA.Apprenticeships.Web.Candidate/Constants/Pages/SignOutPageMessages.cs
@@ -2,7 +2,7 @@
 {
     public static class SignOutPageMessages
     {
-        public const string SignOutMessageText = "You've successfully signed out. <a href=\"{0}\" rel=\"external\">Finished with this service and give feedback</a>";
+        public const string SignOutMessageText = "You've successfully signed out. <a href=\"{0}\" rel=\"external\" target=\"_blank\">Finished with this service and give feedback</a>";
         public const string SessionTimeoutMessageText = "You've been automatically signed out due to inactivity";
         public const string MustAcceptUpdatedTermsAndConditions = "To use this service you must accept our updated terms and conditions";
     }
