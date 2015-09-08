257d050d09af99d7d2e48819f128bcbac574ffcf
Krister Bone <krister.bone@valtech.co.uk>
2015-09-08 11:34:08 +0100

    Added vanilla recruit website and azure role.

---
+++ src/SFA.Apprenticeships.Web.Recruit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SFA.Apprenticeships.Web.Recruit.Startup))]
+namespace SFA.Apprenticeships.Web.Recruit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
