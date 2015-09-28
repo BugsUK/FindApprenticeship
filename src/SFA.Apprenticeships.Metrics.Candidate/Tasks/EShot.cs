2c1c47e64d470bb9c0d520391639e30192fd3cb2
David Winchurch <david.winchurch@iterativeit.com>
2015-09-28 11:36:58 +0100

    E-Shot date fixes and creation of anonymous spreadsheet

--- src/SFA.Apprenticeships.Metrics.Candidate/Tasks/EShot.cs
+++ src/SFA.Apprenticeships.Metrics.Candidate/Tasks/EShot.cs
@@ -4,6 +4,7 @@
     using System.Collections.Generic;
     using System.Globalization;
     using System.IO;
+    using System.Linq;
     using CsvHelper;
     using Domain.Entities.Candidates;
     using Domain.Entities.Users;
@@ -62,7 +63,9 @@
             AddApplicationMetrics(eShotMetrics);
 
             var fileDateTime = DateTime.UtcNow;
+
             WriteEShotCsv(eShotMetrics, fileDateTime);
+            WriteCompleteAnonymousCsv(eShotMetrics, fileDateTime);
         }
 
         private static void WriteEShotCsv(Dictionary<Guid, EShotMetrics> eShotMetrics, DateTime fileDateTime)
@@ -71,7 +74,18 @@
             var textWriter = new StreamWriter(fileName);
             var csv = new CsvWriter(textWriter);
             csv.Configuration.CultureInfo = CultureInfo.GetCultureInfo("en-GB");
-            csv.WriteRecords(eShotMetrics.Values);
+            csv.WriteRecords(eShotMetrics.Values.Where(m => m.AllowMarketingEmails || m.AllowMarketingEmails));
+            textWriter.Flush();
+            textWriter.Close();
+        }
+
+        private static void WriteCompleteAnonymousCsv(Dictionary<Guid, EShotMetrics> eShotMetrics, DateTime fileDateTime)
+        {
+            var fileName = string.Format("FAA-candidates-complete-anonymous{0}.csv", fileDateTime.ToString("yyyyMMdd"));
+            var textWriter = new StreamWriter(fileName);
+            var csv = new CsvWriter(textWriter);
+            csv.Configuration.CultureInfo = CultureInfo.GetCultureInfo("en-GB");
+            csv.WriteRecords(eShotMetrics.Values.Select(m => (EShotMetricsAnonymous)m));
             textWriter.Flush();
             textWriter.Close();
         }
@@ -114,42 +128,34 @@
                 {
                     if (eShotMetrics.ContainsKey(candidate.EntityId))
                     {
-                        var marketingPreferences = candidate.CommunicationPreferences.MarketingPreferences;
-                        if (marketingPreferences.EnableEmail || (marketingPreferences.EnableText && candidate.CommunicationPreferences.VerifiedMobile))
+                        var metrics = eShotMetrics[candidate.EntityId];
+                        metrics.FirstName = candidate.RegistrationDetails.FirstName;
+                        metrics.LastName = candidate.RegistrationDetails.LastName;
+                        metrics.AddressLine1 = candidate.RegistrationDetails.Address.AddressLine1;
+                        metrics.AddressLine2 = candidate.RegistrationDetails.Address.AddressLine2;
+                        metrics.AddressLine3 = candidate.RegistrationDetails.Address.AddressLine3;
+                        metrics.AddressLine4 = candidate.RegistrationDetails.Address.AddressLine4;
+                        metrics.Postcode = candidate.RegistrationDetails.Address.Postcode;
+                        metrics.DateOfBirth = candidate.RegistrationDetails.DateOfBirth.ToString("dd/MM/yyy");
+                        if (candidate.MonitoringInformation != null)
                         {
-                            //At least one form of marketing communications is enabled
-                            var metrics = eShotMetrics[candidate.EntityId];
-                            metrics.FirstName = candidate.RegistrationDetails.FirstName;
-                            metrics.LastName = candidate.RegistrationDetails.LastName;
-                            metrics.AddressLine1 = candidate.RegistrationDetails.Address.AddressLine1;
-                            metrics.AddressLine2 = candidate.RegistrationDetails.Address.AddressLine2;
-                            metrics.AddressLine3 = candidate.RegistrationDetails.Address.AddressLine3;
-                            metrics.AddressLine4 = candidate.RegistrationDetails.Address.AddressLine4;
-                            metrics.Postcode = candidate.RegistrationDetails.Address.Postcode;
-                            metrics.DateOfBirth = candidate.RegistrationDetails.DateOfBirth.ToString("dd/MM/yyy");
-                            if (candidate.MonitoringInformation != null)
-                            {
-                                metrics.Gender = candidate.MonitoringInformation.Gender;
-                            }
-                            metrics.PhoneNumber = "=\"" + candidate.RegistrationDetails.PhoneNumber + "\"";
-                            metrics.AllowMarketingTexts = candidate.CommunicationPreferences.MarketingPreferences.EnableText && candidate.CommunicationPreferences.VerifiedMobile;
-                            metrics.EmailAddress = candidate.RegistrationDetails.EmailAddress;
-                            metrics.AllowMarketingEmails = candidate.CommunicationPreferences.MarketingPreferences.EnableEmail;
-                            metrics.SubscriberId = candidate.SubscriberId;
-                            if (candidate.MonitoringInformation != null && candidate.MonitoringInformation.Ethnicity != null)
-                            {
-                                metrics.Ethnicity = _ethnicity[candidate.MonitoringInformation.Ethnicity.Value];
-                            }
-                            metrics.DateRegistered = candidate.DateCreated;
-                            if(candidate.ApplicationTemplate.EducationHistory != null) {
-                                metrics.LastSchool = candidate.ApplicationTemplate.EducationHistory.Institution;
-                                metrics.LastSchoolFromYear = candidate.ApplicationTemplate.EducationHistory.FromYear;
-                                metrics.LastSchoolToYear = candidate.ApplicationTemplate.EducationHistory.ToYear;
-                            }
+                            metrics.Gender = candidate.MonitoringInformation.Gender;
                         }
-                        else
+                        metrics.PhoneNumber = "=\"" + candidate.RegistrationDetails.PhoneNumber + "\"";
+                        metrics.AllowMarketingTexts = candidate.CommunicationPreferences.MarketingPreferences.EnableText && candidate.CommunicationPreferences.VerifiedMobile;
+                        metrics.EmailAddress = candidate.RegistrationDetails.EmailAddress;
+                        metrics.AllowMarketingEmails = candidate.CommunicationPreferences.MarketingPreferences.EnableEmail;
+                        metrics.SubscriberId = candidate.SubscriberId;
+                        if (candidate.MonitoringInformation != null && candidate.MonitoringInformation.Ethnicity != null)
                         {
-                            eShotMetrics.Remove(candidate.EntityId);
+                            metrics.Ethnicity = _ethnicity[candidate.MonitoringInformation.Ethnicity.Value];
+                        }
+                        metrics.DateRegistered = candidate.DateCreated;
+                        if (candidate.ApplicationTemplate.EducationHistory != null)
+                        {
+                            metrics.LastSchool = candidate.ApplicationTemplate.EducationHistory.Institution;
+                            metrics.LastSchoolFromYear = candidate.ApplicationTemplate.EducationHistory.FromYear;
+                            metrics.LastSchoolToYear = candidate.ApplicationTemplate.EducationHistory.ToYear;
                         }
                     }
                 }
@@ -205,9 +211,24 @@
         }
     }
 
-    public class EShotMetrics
+    public class EShotMetricsAnonymous
     {
         public UserStatuses Status { get; set; }
+        public string DateOfBirth { get; set; }
+        public Gender? Gender { get; set; }
+        public bool AllowMarketingTexts { get; set; }
+        public bool AllowMarketingEmails { get; set; }
+        public Guid SubscriberId { get; set; }
+        public string Ethnicity { get; set; }
+        public DateTime DateRegistered { get; set; }
+        public DateTime? LastLogin { get; set; }
+        public int ApprenticeshipDraftCount { get; set; }
+        public int ApprenticeshipSubmittedCount { get; set; }
+        public int TraineeshipSubmittedCount { get; set; }
+    }
+
+    public class EShotMetrics : EShotMetricsAnonymous
+    {
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string AddressLine1 { get; set; }
@@ -215,21 +236,10 @@
         public string AddressLine3 { get; set; }
         public string AddressLine4 { get; set; }
         public string Postcode { get; set; }
-        public string DateOfBirth { get; set; }
-        public Gender? Gender { get; set; }
         public string PhoneNumber { get; set; }
-        public bool AllowMarketingTexts { get; set; }
         public string EmailAddress { get; set; }
-        public bool AllowMarketingEmails { get; set; }
-        public Guid SubscriberId { get; set; }
-        public string Ethnicity { get; set; }
-        public DateTime DateRegistered { get; set; }
-        public DateTime? LastLogin { get; set; }
         public string LastSchool { get; set; }
         public int LastSchoolFromYear { get; set; }
         public int LastSchoolToYear { get; set; }
-        public int ApprenticeshipDraftCount { get; set; }
-        public int ApprenticeshipSubmittedCount { get; set; }
-        public int TraineeshipSubmittedCount { get; set; }
     }
 }
