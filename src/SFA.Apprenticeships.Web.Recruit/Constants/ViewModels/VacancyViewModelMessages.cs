f749d29e808590ef983360b7208391c7b6cbe299
David Winchurch <david.winchurch@iterativeit.com>
2015-09-30 16:53:42 +0100

    Label names

---
+++ src/SFA.Apprenticeships.Web.Recruit/Constants/ViewModels/VacancyViewModelMessages.cs
@@ -0,0 +1,85 @@
+﻿namespace SFA.Apprenticeships.Web.Recruit.Constants.ViewModels
+{
+    public class VacancyViewModelMessages
+    {
+        public static class Title
+        {
+            public const string LabelText = "Title";
+        }
+
+        public static class ShortDescription
+        {
+            public const string LabelText = "Short description";
+        }
+
+        public static class WorkingWeek
+        {
+            public const string LabelText = "Working week";
+        }
+
+        public static class WeeklyWage
+        {
+            public const string LabelText = "Weekly wage (£)";
+        }
+
+        public static class Duration
+        {
+            public const string LabelText = "Duration (years)";
+        }
+
+        public static class PublishDate
+        {
+            public const string LabelText = "Publish date";
+        }
+
+        public static class ClosingDate
+        {
+            public const string LabelText = "Closing date";
+        }
+
+        public static class PossibleStartDate
+        {
+            public const string LabelText = "Possible start date";
+        }
+
+        public static class LongDescription
+        {
+            public const string LabelText = "Long description";
+        }
+
+        public static class DesiredSkills
+        {
+            public const string LabelText = "Desired skills";
+        }
+
+        public static class FutureProspects
+        {
+            public const string LabelText = "Future prospects";
+        }
+
+        public static class PersonalQualities
+        {
+            public const string LabelText = "Personal qualities";
+        }
+
+        public static class ThingsToConsider
+        {
+            public const string LabelText = "Things to consider";
+        }
+
+        public static class DesiredQualifications
+        {
+            public const string LabelText = "Desired qualifications";
+        }
+
+        public static class FirstQuestion
+        {
+            public const string LabelText = "First question (optional)";
+        }
+
+        public static class SecondQuestion
+        {
+            public const string LabelText = "Second question (optional)";
+        }
+    }
+}
--- src/SFA.Apprenticeships.Web.Recruit/ViewModels/Vacancy/VacancyViewModel.cs
+++ src/SFA.Apprenticeships.Web.Recruit/ViewModels/Vacancy/VacancyViewModel.cs
@@ -1,27 +1,45 @@
 ﻿namespace SFA.Apprenticeships.Web.Recruit.ViewModels.Vacancy
 {
     using System;
+    using System.ComponentModel.DataAnnotations;
+    using Constants.ViewModels;
     using Domain.Entities.Vacancies.Apprenticeships;
 
     public class VacancyViewModel
     {
-        public string Title { get; set; }
-        public string ShortDescription { get; set; }
         public long VacancyReferenceNumber { get; set; }
+        [Display(Name = VacancyViewModelMessages.Title.LabelText)]
+        public string Title { get; set; }
+        [Display(Name = VacancyViewModelMessages.ShortDescription.LabelText)]
+        public string ShortDescription { get; set; }
+        [Display(Name = VacancyViewModelMessages.WorkingWeek.LabelText)]
         public string WorkingWeek { get; set; }
+        [Display(Name = VacancyViewModelMessages.WeeklyWage.LabelText)]
         public string WeeklyWage { get; set; }
+        [Display(Name = VacancyViewModelMessages.Duration.LabelText)]
         public int Duration { get; set; }
+        [Display(Name = VacancyViewModelMessages.PublishDate.LabelText)]
         public DateTime PublishDate { get; set; }
+        [Display(Name = VacancyViewModelMessages.ClosingDate.LabelText)]
         public DateTime ClosingDate { get; set; }
+        [Display(Name = VacancyViewModelMessages.PossibleStartDate.LabelText)]
         public DateTime PossibleStartDate { get; set; }
         public ApprenticeshipLevel ApprenticeshipLevel { get; set; }
+        [Display(Name = VacancyViewModelMessages.LongDescription.LabelText)]
         public string LongDescription { get; set; }
+        [Display(Name = VacancyViewModelMessages.DesiredSkills.LabelText)]
         public string DesiredSkills { get; set; }
+        [Display(Name = VacancyViewModelMessages.FutureProspects.LabelText)]
         public string FutureProspects { get; set; }
+        [Display(Name = VacancyViewModelMessages.PersonalQualities.LabelText)]
         public string PersonalQualities { get; set; }
+        [Display(Name = VacancyViewModelMessages.ThingsToConsider.LabelText)]
         public string ThingsToConsider { get; set; }
+        [Display(Name = VacancyViewModelMessages.DesiredQualifications.LabelText)]
         public string DesiredQualifications { get; set; }
+        [Display(Name = VacancyViewModelMessages.FirstQuestion.LabelText)]
         public string FirstQuestion { get; set; }
+        [Display(Name = VacancyViewModelMessages.SecondQuestion.LabelText)]
         public string SecondQuestion { get; set; }
         public EmployerViewModel Employer { get; set; }
     }
