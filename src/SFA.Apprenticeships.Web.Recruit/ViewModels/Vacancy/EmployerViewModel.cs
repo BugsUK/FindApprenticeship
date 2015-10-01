282b8243d86409d68d2c8753251b755e37cb3734
David Winchurch <david.winchurch@iterativeit.com>
2015-10-01 16:33:20 +0100

    Adding properties to the employer view model

--- src/SFA.Apprenticeships.Web.Recruit/ViewModels/Vacancy/EmployerViewModel.cs
+++ src/SFA.Apprenticeships.Web.Recruit/ViewModels/Vacancy/EmployerViewModel.cs
@@ -1,7 +1,13 @@
 ﻿namespace SFA.Apprenticeships.Web.Recruit.ViewModels.Vacancy
 {
+    using Common.ViewModels.Locations;
+
     public class EmployerViewModel
     {
         public string Ern { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Website { get; set; }
+        public AddressViewModel Address { get; set; }
     }
 }
