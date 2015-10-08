6a5d32d25d02e45fd2a0515c9e9d9d45790ee415
David Winchurch <david.winchurch@iterativeit.com>
2015-10-08 16:48:08 +0100

    Added line break

--- src/SFA.Apprenticeships.Web.Recruit/Constants/Messages/AuthorizeMessages.cs
+++ src/SFA.Apprenticeships.Web.Recruit/Constants/Messages/AuthorizeMessages.cs
@@ -7,7 +7,7 @@
         public const string MissingServicePermission = "You do not have permission to access this service. Please contact helpdesk";
         public const string NoProviderProfile = "As you're the first person to sign in from your organisation, please start by adding your provider name";
         public const string FailedMinimumSitesCountCheck = "Please create at least one site";
-        public const string FirstUser = "As you're the first person to sign in from your organisation, please take a moment to review your training sites. Please email our <a href=\"mailto:nationalhelpdesk@findapprenticeship.service.gov.uk\">helpdesk</a> if any of your details need to be changed";
+        public const string FirstUser = "As you're the first person to sign in from your organisation, please take a moment to review your training sites.<br/>Please email our <a href=\"mailto:nationalhelpdesk@findapprenticeship.service.gov.uk\">helpdesk</a> if any of your details need to be changed";
         public const string NoUserProfile = "Please fill in your user profile";
         public const string EmailAddressNotVerified = "Please verify your email address";
         public const string SignedOut = "You have successfully signed out";
