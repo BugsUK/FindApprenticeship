9a36445809ae201f98887d9a11117df3cef3e991
alangorton <alan.gorton@valtech.co.uk>
2015-10-20 14:00:34 +0100

    EDS: map from BS Address to SFA format address.

--- src/SFA.Apprenticeships.Infrastructure.EmployerDataService/Mappers/BSAddressMapper.cs
+++ src/SFA.Apprenticeships.Infrastructure.EmployerDataService/Mappers/BSAddressMapper.cs
@@ -1,28 +1,43 @@
 ﻿namespace SFA.Apprenticeships.Infrastructure.EmployerDataService.Mappers
 {
+    using System;
+    using System.Linq;
     using Domain.Entities.Locations;
     using EmployerDataService;
 
-    // http://xml.coverpages.org/BS7666SchemaDefinition-20020312-xsd.txt
     public class BsAddressMapper
     {
         public Address ToAddress(BSaddressStructure fromAddress)
         {
             if (fromAddress == null)
             {
-                return null;
+                throw new ArgumentNullException(nameof(fromAddress));
             }
+
+            // Reference: http://xml.coverpages.org/BS7666SchemaDefinition-20020312-xsd.txt.
+            //  PAON: Primary Addressable Object Name
+            //  SAON: Secondary Addressable Object Name
+            var paon = fromAddress.PAON?.Items.FirstOrDefault()?.ToString();
 
             return new Address
             {
-                AddressLine1 = fromAddress.StreetDescription,
-                AddressLine2 = fromAddress.PostTown,
-                AddressLine3 = null,
+                AddressLine1 = MapAddressComponent(paon),
+                AddressLine2 = MapAddressComponent(fromAddress.StreetDescription),
+                AddressLine3 = MapAddressComponent(fromAddress.PostTown),
                 AddressLine4 = null,
-                Postcode = fromAddress.PostCode,
-                Uprn = fromAddress.UniquePropertyReferenceNumber,
+                Postcode = MapAddressComponent(fromAddress.PostCode),
+                Uprn = null,
                 GeoPoint = null
             };
         }
+
+        #region Helpers
+
+        private static string MapAddressComponent(string addressComponent)
+        {
+            return string.IsNullOrWhiteSpace(addressComponent) ? null : addressComponent.Trim();
+        }
+
+        #endregion
     }
 }
--- src/SFA.Apprenticeships.Infrastructure.UnitTests/EmployerDataService/Mappers/BsAddressMapperTests.cs
+++ src/SFA.Apprenticeships.Infrastructure.UnitTests/EmployerDataService/Mappers/BsAddressMapperTests.cs
@@ -1,19 +1,171 @@
 ﻿namespace SFA.Apprenticeships.Infrastructure.UnitTests.EmployerDataService.Mappers
 {
+    using System;
+    using FluentAssertions;
+    using Infrastructure.EmployerDataService.EmployerDataService;
+    using Infrastructure.EmployerDataService.Mappers;
     using NUnit.Framework;
+
+    /*
+        Sample Address from EDS:
+
+        "Address": {
+            "SAON": null,
+            "PAON": {
+                "Items": [
+                    "Arena Retail Park Unit 2"
+                ],
+                    "ItemsElementName": [
+                    "Description"
+                ]
+            },
+            "StreetDescription": "Classic Drive",
+            "UniqueStreetReferenceNumber": null,
+            "Items": null,
+            "ItemsElementName": null,
+            "PostTown": "Coventry",
+            "PostCode": "CV6 6AS",
+            "UniquePropertyReferenceNumber": null // NOTE: always appears to be null
+        }
+    */
 
     [TestFixture]
     public class BsAddressMapperTests
     {
-        [Test]
-        public void ShouldMapBsAddress()
+        private BsAddressMapper _mapper;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mapper = new BsAddressMapper();
+        }
+
+        [TestCase(" Arena Retail Park Unit 2 ", false)]
+        [TestCase("", true)]
+        [TestCase(" ", true)]
+        [TestCase(null, true)]
+        public void ShouldMapPrimaryAddressableObjectNameToAddressLine1(string paon, bool expectNullAddressLine1ToBe)
         {
             // Arrange.
+            var fromAddress = new BSaddressStructure
+            {
+                PAON = new AONstructure
+                {
+                    Items = new object[]
+                    {
+                        paon
+                    }
+                }
+            };
 
             // Act.
+            var toAddress = _mapper.ToAddress(fromAddress);
 
             // Assert.
-            //Assert.Fail();
+            toAddress.Should().NotBeNull();
+
+            if (expectNullAddressLine1ToBe)
+            {
+                toAddress.AddressLine1.Should().BeNull();
+            }
+            else
+            {
+                toAddress.AddressLine1.Should().Be(paon.Trim());
+            }
+        }
+
+        [TestCase(" Classic Drive ", false)]
+        [TestCase("", true)]
+        [TestCase(" ", true)]
+        [TestCase(null, true)]
+        public void ShouldMapStreetDescriptionToAddressLine2(string streetDescription, bool expectNullAddressLine2)
+        {
+            // Arrange.
+            var fromAddress = new BSaddressStructure
+            {
+                StreetDescription = streetDescription
+            };
+
+            // Act.
+            var toAddress = _mapper.ToAddress(fromAddress);
+
+            // Assert.
+            toAddress.Should().NotBeNull();
+
+            if (expectNullAddressLine2)
+            {
+                toAddress.AddressLine2.Should().BeNull();
+            }
+            else
+            {
+                toAddress.AddressLine2.Should().Be(streetDescription.Trim());
+            }
+        }
+
+        [TestCase(" Coventry ", false)]
+        [TestCase("", true)]
+        [TestCase(" ", true)]
+        [TestCase(null, true)]
+        public void ShouldMapPostTownToAddressLine3(string postTown, bool expectNullAddressLine3)
+        {
+            // Arrange.
+            var fromAddress = new BSaddressStructure
+            {
+                PostTown = postTown
+            };
+
+            // Act.
+            var toAddress = _mapper.ToAddress(fromAddress);
+
+            // Assert.
+            toAddress.Should().NotBeNull();
+
+            if (expectNullAddressLine3)
+            {
+                toAddress.AddressLine3.Should().BeNull();
+            }
+            else
+            {
+                toAddress.AddressLine3.Should().Be(postTown.Trim());
+            }
+        }
+
+        [TestCase(" CV6 6AS ", false)]
+        [TestCase("", true)]
+        [TestCase(" ", true)]
+        [TestCase(null, true)]
+        public void ShouldMapPostcode(string postcode, bool expectNullPostcode)
+        {
+            // Arrange.
+            var fromAddress = new BSaddressStructure
+            {
+                PostCode = postcode
+            };
+
+            // Act.
+            var toAddress = _mapper.ToAddress(fromAddress);
+
+            // Assert.
+            toAddress.Should().NotBeNull();
+
+            if (expectNullPostcode)
+            {
+                toAddress.Postcode.Should().BeNull();
+            }
+            else
+            {
+                toAddress.Postcode.Should().Be(postcode.Trim());
+            }
+        }
+
+        [Test]
+        public void ShouldThrowWhenFromAddressIsNull()
+        {
+            // Act.
+            Action action = () => _mapper.ToAddress(default(BSaddressStructure));
+
+            // Assert.
+            action.ShouldThrow<ArgumentNullException>();
         }
     }
 }
