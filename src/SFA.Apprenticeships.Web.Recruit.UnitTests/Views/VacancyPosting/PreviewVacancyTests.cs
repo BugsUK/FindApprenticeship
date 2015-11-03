0fb8f4f9364530c8bf31a6edd6b8e819dbb1e120
valtechovais <ovais.oozeer@valtech.co.uk>
2015-11-03 16:11:58 +0000

    a couple of tests around wage display

--- src/SFA.Apprenticeships.Web.Recruit.UnitTests/Views/VacancyPosting/PreviewVacancyTests.cs
+++ src/SFA.Apprenticeships.Web.Recruit.UnitTests/Views/VacancyPosting/PreviewVacancyTests.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Web.Mvc;
-using FluentAssertions;
-using NUnit.Framework;
-using RazorGenerator.Testing;
-using SFA.Apprenticeships.Web.Common.ViewModels.Locations;
-using SFA.Apprenticeships.Web.Recruit.ViewModels.Provider;
-using SFA.Apprenticeships.Web.Recruit.ViewModels.Vacancy;
-using SFA.Apprenticeships.Web.Recruit.Views.VacancyPosting;
-
-namespace SFA.Apprenticeships.Web.Recruit.UnitTests.Views.VacancyPosting
+﻿namespace SFA.Apprenticeships.Web.Recruit.UnitTests.Views.VacancyPosting
 {
     using Common.ViewModels;
+    using System;
+    using System.Collections.Generic;
+    using System.Web.Mvc;
+    using FluentAssertions;
+    using NUnit.Framework;
+    using RazorGenerator.Testing;
+    using Domain.Entities.Vacancies.ProviderVacancies;
+    using Common.ViewModels.Locations;
+    using ViewModels.Provider;
+    using ViewModels.Vacancy;
+    using Recruit.Views.VacancyPosting;
 
     [TestFixture]
     public class PreviewVacancyTests : ViewUnitTest
@@ -49,6 +49,85 @@ namespace SFA.Apprenticeships.Web.Recruit.UnitTests.Views.VacancyPosting
             var view = details.RenderAsHtml(viewModel);
 
             view.GetElementbyId("dashboardLink").Should().NotBeNull("Should exists a return to dashboard button");
+        }
+
+        [TestCase(WageType.NationalMinimumWage, "National Minimum Wage")]
+        [TestCase(WageType.ApprenticeshipMinimumWage, "National Minimum Wage for apprentices")]
+        public void ShouldShowWageText(WageType wagetype, string expectedDisplayText)
+        {
+            var details = new PreviewVacancy();
+
+            var viewModel = new VacancyViewModel()
+            {
+                ApprenticeshipLevels = new List<SelectListItem>(),
+                ProviderSite = new ProviderSiteViewModel()
+                {
+                    Address = new AddressViewModel()
+                },
+                VacancySummaryViewModel = new VacancySummaryViewModel
+                {
+                    ClosingDate = new DateViewModel(DateTime.Now),
+                    PossibleStartDate = new DateViewModel(DateTime.Now),
+                    WageType = wagetype
+                },
+                NewVacancyViewModel = new NewVacancyViewModel
+                {
+                    ProviderSiteEmployerLink = new ProviderSiteEmployerLinkViewModel()
+                    {
+                        Employer = new EmployerViewModel()
+                        {
+                            Address = new AddressViewModel()
+                        }
+                    }
+                },
+                VacancyQuestionsViewModel = new VacancyQuestionsViewModel(),
+                VacancyRequirementsProspectsViewModel = new VacancyRequirementsProspectsViewModel()
+            };
+
+            var view = details.RenderAsHtml(viewModel);
+
+            view.GetElementbyId("vacancy-wage").InnerHtml.Should().Be(expectedDisplayText);
+        }
+
+        [TestCase(100, WageUnit.Weekly, @"&#163;100")]
+        [TestCase(100, WageUnit.Monthly, @"&#163;100")]
+        [TestCase(100, WageUnit.Annually, @"&#163;100")]
+        public void ShouldShowCustomWageAmount(decimal wage, WageUnit wageUnit, string expectedDisplayText)
+        {
+            var details = new PreviewVacancy();
+
+            var viewModel = new VacancyViewModel()
+            {
+                ApprenticeshipLevels = new List<SelectListItem>(),
+                ProviderSite = new ProviderSiteViewModel()
+                {
+                    Address = new AddressViewModel()
+                },
+                VacancySummaryViewModel = new VacancySummaryViewModel
+                {
+                    ClosingDate = new DateViewModel(DateTime.Now),
+                    PossibleStartDate = new DateViewModel(DateTime.Now),
+                    WageType = WageType.Custom,
+                    WageUnit = wageUnit,
+                    Wage = wage
+                },
+                NewVacancyViewModel = new NewVacancyViewModel
+                {
+                    ProviderSiteEmployerLink = new ProviderSiteEmployerLinkViewModel()
+                    {
+                        Employer = new EmployerViewModel()
+                        {
+                            Address = new AddressViewModel()
+                        }
+                    }
+                },
+                VacancyQuestionsViewModel = new VacancyQuestionsViewModel(),
+                VacancyRequirementsProspectsViewModel = new VacancyRequirementsProspectsViewModel()
+            };
+
+            var view = details.RenderAsHtml(viewModel);
+
+            view.GetElementbyId("vacancy-wage").InnerHtml.Should().Be(expectedDisplayText);
         }
     }
 }
