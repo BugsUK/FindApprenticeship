95f2cb665e0fdca950a50fac48973e266225d988
David Winchurch <david.winchurch@iterativeit.com>
2015-11-04 15:52:54 +0000

    Standards in the database project

---
+++ src/SFA.Apprenticeships.Data.Reference/dbo/Tables/ApprenticeshipStandard.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ApprenticeshipStandard]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [ApprenticeshipSectorId] INT NOT NULL, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [ApprenticeshipLevel] INT NOT NULL, 
+    CONSTRAINT [FK_ApprenticeshipStandard_ApprenticeshipSector] FOREIGN KEY ([ApprenticeshipSectorId]) REFERENCES [dbo].[ApprenticeshipSector]([Id])
+)
