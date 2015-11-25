ba94dec57fc9660e2ad008ce0aaeb8e9ffe7a43c
valtechovais <ovais.oozeer@valtech.co.uk>
2015-11-25 15:15:59 +0000

    test push new prototype controller

---
+++ src/SFA.Apprenticeships.Web.Recruit/Controllers/PrototypeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SFA.Apprenticeships.Web.Recruit.Controllers
+{
+    public class PrototypeController : Controller
+    {
+        // GET: Prototype
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
