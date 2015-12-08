19aa75d6b8a3e70acb598df3f79f4cb6cc7e1084
David Winchurch <david.winchurch@iterativeit.com>
2015-12-08 15:54:12 +0000

    Added WebProxy schema and WebProxyConsumer table

---
+++ src/SFA.Apprenticeships.Data/Schemas/WebProxy/Tables/WebProxy.WebProxyConsumer.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [WebProxy].[WebProxyConsumer]
+(
+	[WebProxyConsumerId] [int] IDENTITY(1,1) NOT NULL,
+	[ExternalSystemId] [uniqueidentifier] NOT NULL,
+	[ShortDescription] [nvarchar](50) NOT NULL,
+	[FullDescription] [nvarchar](max) NOT NULL,
+	[RouteToCompatabilityWebServiceRegex] [nvarchar](max) NOT NULL
+    CONSTRAINT [PK_WebProxyConsumer] PRIMARY KEY ([WebProxyConsumerId])
+)
