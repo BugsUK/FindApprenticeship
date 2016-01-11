b6bdbd6ca8c853658c6d0b613ae998591f52efa1
Vicenç García Altés <vicenc.garcia-altes@valtech.co.uk>
2016-01-11 22:51:51 +0000

    Created database initializer

--- src/SFA.Apprenticeships.Infrastructure.Repositories.Vacancies.Sql.Tests/Class1.cs
+++ src/SFA.Apprenticeships.Infrastructure.Repositories.Vacancies.Sql.Tests/Class1.cs
@@ -11,6 +11,16 @@ namespace SFA.Apprenticeships.Infrastructure.Repositories.Vacancies.Sql.Tests
     [TestFixture]
     public class Class1
     {
+        [SetUp]
+        public void SetUp()
+        {
+            var solutionDirectory = AppDomain.CurrentDomain.BaseDirectory + "\\..\\..\\..\\SFA.Apprenticeships.Data";
+            var connectionString = "Server=VTUK027\\SQLEXPRESS;Database=Raa2;Trusted_Connection=True;"; //TODO: get from settings
+            var databaseName = "Raa2";
+            var dbInitialiser = new DatabaseInitialiser(solutionDirectory, connectionString, databaseName);
+            dbInitialiser.Publish(true);    
+        }
+
         [Test]
         public void DoMappersMapEverything()
         {
---
+++ src/SFA.Apprenticeships.Infrastructure.Repositories.Vacancies.Sql.Tests/DatabaseInitialiser.cs
@@ -0,0 +1,51 @@
+﻿namespace SFA.Apprenticeships.Infrastructure.Repositories.Vacancies.Sql.Tests
+{
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+    using Microsoft.SqlServer.Dac;
+
+    public class DatabaseInitialiser
+    {
+        private readonly string _databaseProjectPath;
+        private readonly string _connectionString;
+        private readonly string _databaseTargetName;
+
+        public DatabaseInitialiser(string databaseProjectPath, string connectionString, string databaseTargetName)
+        {
+            _databaseProjectPath = databaseProjectPath;
+            _connectionString = connectionString;
+            _databaseTargetName = databaseTargetName;
+        }
+
+        public void Publish(bool dropDatabase)
+        {
+            var dacServices = new DacServices(_connectionString);
+
+            //Wire up events for Deploy messages and for task progress (For less verbose output, don't subscribe to Message Event (handy for debugging perhaps?)
+            dacServices.Message += new EventHandler<DacMessageEventArgs>(dacServices_Message);
+            dacServices.ProgressChanged += new EventHandler<DacProgressEventArgs>(dacServices_ProgressChanged);
+
+            var databaseProjectName = Path.GetFileName(_databaseProjectPath);
+            var snapshotPath = Path.Combine(_databaseProjectPath + $"\\bin\\Local\\{databaseProjectName}.dacpac"); //configure Local
+            var dbPackage = DacPackage.Load(snapshotPath);
+
+            var dbDeployOptions = new DacDeployOptions();
+            //Cut out a lot of options here for configuring deployment, but are all part of DacDeployOptions
+            dbDeployOptions.SqlCommandVariableValues.Add("debug", "false");
+            dbDeployOptions.CreateNewDatabase = dropDatabase;
+            
+            dacServices.Deploy(dbPackage, _databaseTargetName, true, dbDeployOptions);
+        }
+
+        static void dacServices_Message(object sender, DacMessageEventArgs e)
+        {
+            Debug.WriteLine("DAC Message: {0}", e.Message);
+        }
+
+        static void dacServices_ProgressChanged(object sender, DacProgressEventArgs e)
+        {
+            Debug.WriteLine(e.Status + ": " + e.Message);
+        }
+    }
+}
