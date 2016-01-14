90604f0f6d840550903c77989197e0e57b2c8ee6
valtechovais <ovais.oozeer@valtech.co.uk>
2016-01-14 11:50:28 +0000

    WIP postal address lookup bits.  Interfaces added, unit tests not yet.

---
+++ src/SFA.Apprenticeships.Application.Location/PostalAddressSearchService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFA.Apprenticeships.Application.Location
+{
+    using Domain.Entities.Locations;
+    using Interfaces.Locations;
+    public class PostalAddressSearchService : IPostalAddressSearchService
+    {
+        public PostalAddress GetAddress(string fullPostcode, string addressLine1)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/SFA.Apprenticeships.Infrastructure.Postcode/PostalAddressLookupProvider.cs
@@ -0,0 +1,31 @@
+﻿namespace SFA.Apprenticeships.Infrastructure.Postcode
+{
+    using System;
+    using System.Collections.Generic;
+    using Application.Interfaces.Logging;
+    using Application.Location;
+    using Configuration;
+    using Domain.Entities.Locations;
+    using Domain.Interfaces.Configuration;
+    using Rest;
+
+    public class PostalAddressLookupProvider : RestService, IPostalAddressLookupProvider
+    {
+        private readonly ILogService _logger;
+        private readonly RetrieveAddressService _retrieveAddressService;
+        private AddressConfiguration Config { get; }
+
+        public PostalAddressLookupProvider(IConfigurationService configurationService, ILogService logger, RetrieveAddressService retrieveAddressService)
+        {
+            _logger = logger;
+            _retrieveAddressService = retrieveAddressService;
+            Config = configurationService.Get<AddressConfiguration>();
+            BaseUrl = new Uri(Config.RetrieveServiceEndpoint);
+        }
+
+        public IEnumerable<PostalAddress> GetPostalAddresses(string addressLine1, string postcode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
