79198489b09135095a33724b3b5ee02c34de2c6b
David Winchurch <david.winchurch@iterativeit.com>
2016-01-18 17:08:16 +0000

    Repository code and initial unit and integration test work

---
+++ src/SFA.Apprenticeships.Infrastructure.Repositories.Reference/SqlReferenceRepository.cs
@@ -0,0 +1,35 @@
+﻿namespace SFA.Apprenticeships.Infrastructure.Repositories.Reference
+{
+    using System.Collections.Generic;
+    using Domain.Entities.Reference;
+    using Domain.Interfaces.Repositories;
+    using SFA.Infrastructure.Interfaces;
+    using SFA.Infrastructure.Sql;
+
+    public class SqlReferenceRepository : IReferenceRepository
+    {
+        private readonly IGetOpenConnection _getOpenConnection;
+        private readonly IMapper _mapper;
+        private readonly ILogService _logger;
+
+        public SqlReferenceRepository(IGetOpenConnection getOpenConnection, IMapper mapper, ILogService logger)
+        {
+            _getOpenConnection = getOpenConnection;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public IList<County> GetCounties()
+        {
+            _logger.Debug("Calling database to get all counties");
+
+            var dbCounties = _getOpenConnection.Query<NewDB.Domain.Entities.Reference.County>(@"SELECT * FROM Reference.Counties ORDER BY FullName");
+
+            _logger.Debug($"Found {dbCounties.Count} counties");
+
+            var counties = _mapper.Map<IList<NewDB.Domain.Entities.Reference.County>, IList<County>>(dbCounties);
+
+            return counties;
+        }
+    }
+}
