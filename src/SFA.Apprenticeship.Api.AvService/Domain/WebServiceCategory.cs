57e464691ff7409f8e0796e443e18fdd05726630
alangorton <alan.gorton@valtech.co.uk>
2016-01-21 14:02:44 +0000

    US876: remove Flags from WebServiceCategory enum.

--- src/SFA.Apprenticeship.Api.AvService/Domain/WebServiceCategory.cs
+++ src/SFA.Apprenticeship.Api.AvService/Domain/WebServiceCategory.cs
@@ -2,13 +2,12 @@
 {
     using System;
 
-    [Flags]
     public enum WebServiceCategory
     {
         Unknown,
         Reference = 1,
         VacancyUpload = 2,
-        VacancySummary = 4,
-        VacancyDetail = 8
+        VacancySummary = 3,
+        VacancyDetail = 4
     }
 }
