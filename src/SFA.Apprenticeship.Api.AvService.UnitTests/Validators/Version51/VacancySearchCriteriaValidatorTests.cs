a33c996328aa52deec1688a294e66b381ef2f4bb
alangorton <alan.gorton@valtech.co.uk>
2016-01-22 14:16:28 +0000

    US876: added skeleton validator unit tests.

---
+++ src/SFA.Apprenticeship.Api.AvService.UnitTests/Validators/Version51/VacancySearchCriteriaValidatorTests.cs
@@ -0,0 +1,32 @@
+﻿namespace SFA.Apprenticeship.Api.AvService.UnitTests.Validators.Version51
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class VacancySearchCriteriaValidatorTests
+    {
+        [Test]
+        [Ignore]
+        public void ShouldRequireVacancyLocationType()
+        {
+            // Arrange.
+
+            // Act.
+
+            // Assert.
+            Assert.Fail();
+        }
+
+        [Test]
+        [Ignore]
+        public void ShouldRequirePageIndex()
+        {
+            // Arrange.
+
+            // Act.
+
+            // Assert.
+            Assert.Fail();
+        }
+    }
+}
---
+++ src/SFA.Apprenticeship.Api.AvService/Validators/Version51/VacancySearchCriteriaValidator.cs
@@ -0,0 +1,9 @@
+﻿namespace SFA.Apprenticeship.Api.AvService.Validators.Version51
+{
+    using FluentValidation;
+    using MessageContracts.Version51;
+
+    public class VacancySearchCriteriaValidator : AbstractValidator<VacancyUploadRequest>
+    {
+    }
+}
