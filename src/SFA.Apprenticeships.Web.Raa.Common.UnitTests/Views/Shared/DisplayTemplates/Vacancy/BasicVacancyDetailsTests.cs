a7f0e79e4cd356ed2ec27e9a5a7aaf5e9cdf74ad
David Winchurch <david.winchurch@iterativeit.com>
2016-02-04 15:52:07 +0000

    Vacancy type selector on basic details page

---
+++ src/SFA.Apprenticeships.Web.Raa.Common.UnitTests/Views/Shared/DisplayTemplates/Vacancy/BasicVacancyDetailsTests.cs
@@ -0,0 +1,28 @@
+﻿namespace SFA.Apprenticeships.Web.Raa.Common.UnitTests.Views.Shared.DisplayTemplates.Vacancy
+{
+    using FluentAssertions;
+    using NUnit.Framework;
+    using RazorGenerator.Testing;
+
+    [TestFixture]
+    public class BasicVacancyDetailsTests : BasicVacancyDetailsTestsBase
+    {
+        [Test]
+        public void ShouldHaveVacancyTypeSelector()
+        {
+            var view = Details.RenderAsHtml(ViewModel);
+
+            view.GetElementbyId("vacancy-type-apprenticeship").Should().NotBeNull();
+            view.GetElementbyId("vacancy-type-traineeship").Should().NotBeNull();
+        }
+
+        [Test]
+        public void VacancyTypeSelectorsLabel()
+        {
+            var view = Details.RenderAsHtml(ViewModel);
+
+            view.GetElementbyId("vacancy-type-apprenticeship").NextSibling.InnerText.Should().Contain("Apprenticeship");
+            view.GetElementbyId("vacancy-type-traineeship").NextSibling.InnerText.Should().Contain("Traineeship");
+        }
+    }
+}
---
+++ src/SFA.Apprenticeships.Web.Raa.Common.UnitTests/Views/Shared/DisplayTemplates/Vacancy/BasicVacancyDetailsTestsBase.cs
@@ -0,0 +1,12 @@
+﻿namespace SFA.Apprenticeships.Web.Raa.Common.UnitTests.Views.Shared.DisplayTemplates.Vacancy
+{
+    using Common.Views.Shared.DisplayTemplates.Vacancy;
+    using Ploeh.AutoFixture;
+    using ViewModels.Vacancy;
+
+    public abstract class BasicVacancyDetailsTestsBase : ViewUnitTest
+    {
+        protected NewVacancyViewModel ViewModel = new Fixture().Build<NewVacancyViewModel>().Create();
+        protected BasicVacancyDetails Details = new BasicVacancyDetails();
+    }
+}
