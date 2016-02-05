45771812b5deaa74b2a3f14d7f4175bc9137d43f
Vicenç García Altés <vicenc.garcia-altes@valtech.co.uk>
2016-02-05 17:10:53 +0000

    Removed Full text search from AvmsPlus

--- src/SFA.Apprenticeships.Data.AvmsPlus/dbo/Stored Procedures/uspSchoolSearch.sql
+++ src/SFA.Apprenticeships.Data.AvmsPlus/dbo/Stored Procedures/uspSchoolSearch.sql
@@ -1,35 +1,35 @@
-﻿CREATE PROCEDURE [dbo].[uspSchoolSearch]       
- @SchoolName NVARCHAR (1000),
- @SchoolLocality NVARCHAR (1000)  
-AS      
-BEGIN      
+﻿--CREATE PROCEDURE [dbo].[uspSchoolSearch]       
+-- @SchoolName NVARCHAR (1000),
+-- @SchoolLocality NVARCHAR (1000)  
+--AS      
+--BEGIN      
       
- SET NOCOUNT ON      
-    Select   
-        SchoolId,  
-        URN,  
-        SchoolName,  
-        Address1,
-        Address2,
-        Area,
-        Town,
-        County,
-        Postcode
+-- SET NOCOUNT ON      
+--    Select   
+--        SchoolId,  
+--        URN,  
+--        SchoolName,  
+--        Address1,
+--        Address2,
+--        Area,
+--        Town,
+--        County,
+--        Postcode
           
-    FROM 
-        School  
-    WHERE
+--    FROM 
+--        School  
+--    WHERE
             
-    (
-        Address2 LIKE @SchoolLocality + '%'
-    OR  Area LIKE @SchoolLocality + '%'
-    OR  Town LIKE @SchoolLocality + '%'
-    OR  County LIKE @SchoolLocality + '%'
-    OR  Postcode LIKE @SchoolLocality + '%'
-    )
-    AND CONTAINS(*, @SchoolName)
+--    (
+--        Address2 LIKE @SchoolLocality + '%'
+--    OR  Area LIKE @SchoolLocality + '%'
+--    OR  Town LIKE @SchoolLocality + '%'
+--    OR  County LIKE @SchoolLocality + '%'
+--    OR  Postcode LIKE @SchoolLocality + '%'
+--    )
+--    AND CONTAINS(*, @SchoolName)
 
      
-SET NOCOUNT OFF      
+--SET NOCOUNT OFF      
       
-END
+--END
