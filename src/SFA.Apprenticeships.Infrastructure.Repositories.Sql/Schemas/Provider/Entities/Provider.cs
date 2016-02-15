2bcd4def923f574593e8d325ab0a9b4c85a05c12
alangorton <alan.gorton@valtech.co.uk>
2016-02-15 11:03:54 +0000

    US1221: added Provider and ProviderUser repo integration tests. Ignored for now.

--- src/SFA.Apprenticeships.Infrastructure.Repositories.Sql/Schemas/Provider/Entities/Provider.cs
+++ src/SFA.Apprenticeships.Infrastructure.Repositories.Sql/Schemas/Provider/Entities/Provider.cs
@@ -4,59 +4,38 @@
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
 
-    // TODO: SQL: AG: remove unused fields.
-
     [Table("dbo.Provider")]
     public class Provider
     {
         [Key]
         public int ProviderId { get; set; }
 
-        // [Required]
-        // public int Upin { get; set; }
+        public int Upin { get; set; }
 
         [Required]
         public int Ukprn { get; set; }
 
         public string FullName { get; set; }
 
-        // public string TradingName { get; set; }
-
-        // [Required]
-        // public bool IsContracted { get; set; }
-
-        // [Column(TypeName = "datetime2")]
-        // public DateTime ContractedFrom { get; set; }
-
-        // [Column(TypeName = "datetime2")]
-        // public DateTime ContractedTo { get; set; }
-
-        // [Required]
-        // public int ProviderStatusTypeId { get; set; }
-
-        // [Required]
-        // [Column(TypeName = "datetime2")]
-        // public bool IsNasProvider { get; set; }
-
-        // [Column(TypeName = "datetime2")]
-        // public int OriginalUpin { get; set; }
-
-        // TODO: SQL: AG: remove dead code below.
-
-        /*
-        public int ProviderId { get; set; }
-
-        [Column(TypeName = "datetime2")]
-        public DateTime DateCreated { get; set; }
-
-        [Column(TypeName = "datetime2")]
-        public DateTime? DateUpdated { get; set; }
+        public string TradingName { get; set; }
 
         [Required]
-        public int Ukprn { get; set; }
+        public bool IsContracted { get; set; }
+
+        [Column(TypeName = "datetime2")]
+        public DateTime ContractedFrom { get; set; }
+
+        [Column(TypeName = "datetime2")]
+        public DateTime ContractedTo { get; set; }
 
         [Required]
-        public string FullName { get; set; }
-        */
+        public int ProviderStatusTypeId { get; set; }
+
+        [Required]
+        [Column(TypeName = "datetime2")]
+        public bool IsNasProvider { get; set; }
+
+        [Column(TypeName = "datetime2")]
+        public int OriginalUpin { get; set; }
     }
 }
