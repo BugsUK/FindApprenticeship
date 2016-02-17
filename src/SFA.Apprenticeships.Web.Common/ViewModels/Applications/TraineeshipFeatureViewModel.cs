fe23a0bb9f6ee4e0fd760a0c12d0a395fc834e4d
David Winchurch <david.winchurch@iterativeit.com>
2016-02-17 15:53:13 +0000

    First steps in splitting out my applications shared view

---
+++ src/SFA.Apprenticeships.Web.Common/ViewModels/Applications/TraineeshipFeatureViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SFA.Apprenticeships.Web.Common.ViewModels.Applications
+{
+    public class TraineeshipFeatureViewModel
+    {
+        public bool ShowTraineeshipsPrompt { get; set; }
+
+        public bool ShowTraineeshipsLink { get; set; }
+    }
+}
---
+++ src/SFA.Apprenticeships.Web.Common/ViewModels/MyApplications/MyApplicationRoutes.cs
@@ -0,0 +1,12 @@
+﻿namespace SFA.Apprenticeships.Web.Common.ViewModels.MyApplications
+{
+    public class MyApplicationRoutes
+    {
+        public string ApprenticeshipView { get; set; } 
+        public string ApprenticeshipArchive { get; set; } 
+        public string NextSteps { get; set; } 
+        public string ApprenticeshipApply { get; set; } 
+        public string ApprenticeshipDelete { get; set; } 
+        public string TraineeshipView { get; set; } 
+    }
+}
