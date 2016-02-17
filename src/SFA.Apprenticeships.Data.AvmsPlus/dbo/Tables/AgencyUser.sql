a788bede543f1eda8e623819f9bceb70f00a2e16
alangorton <alan.gorton@valtech.co.uk>
2016-02-17 15:33:40 +0000

    US1222: added unique constraint to AgencyUser.Username; handle SQL key violation explicitly.

--- src/SFA.Apprenticeships.Data.AvmsPlus/dbo/Tables/AgencyUser.sql
+++ src/SFA.Apprenticeships.Data.AvmsPlus/dbo/Tables/AgencyUser.sql
@@ -3,6 +3,11 @@
 	[AgencyUserId] INT IDENTITY(1,1), 
     [DateCreated] DATETIME2 NOT NULL, 
     [DateUpdated] DATETIME2 NULL, 
-    [Username] NVARCHAR(MAX) NOT NULL, 
+    [Username] NVARCHAR(100) NOT NULL, 
     CONSTRAINT PK_UserProfile_AgencyUser PRIMARY KEY (AgencyUserId)
 )
+
+GO
+
+
+CREATE UNIQUE INDEX [IX_AgencyUser_Username] ON [UserProfile].[AgencyUser] ([Username])
