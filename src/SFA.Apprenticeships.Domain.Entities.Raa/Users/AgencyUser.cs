572845792e2d044edc56f4c7882cd747cd512ff7
David Winchurch <david.winchurch@iterativeit.com>
2016-03-17 15:56:26 +0000

    US949: AgencyUser Team renamed to RegionalTeam

--- src/SFA.Apprenticeships.Domain.Entities.Raa/Users/AgencyUser.cs
+++ src/SFA.Apprenticeships.Domain.Entities.Raa/Users/AgencyUser.cs
@@ -1,14 +1,15 @@
 ﻿namespace SFA.Apprenticeships.Domain.Entities.Raa.Users
 {
     using System;
+    using Reference;
 
     public class AgencyUser : ICreatableEntity, IUpdatableEntity
     {
         public int AgencyUserId { get; set; }
         public Guid AgencyUserGuid { get; set; }
         public string Username { get; set; }
-        public Team Team { get; set; }
         public Role Role { get; set; }
+        public RegionalTeam RegionalTeam { get; set; }
         public DateTime CreatedDateTime { get; set; }
         public DateTime? UpdatedDateTime { get; set; }
     }
