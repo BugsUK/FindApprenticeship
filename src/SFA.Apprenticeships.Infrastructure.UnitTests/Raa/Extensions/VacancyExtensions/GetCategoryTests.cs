25beeb7ad6055bba1dbebdd605a7193e07878e27
David Winchurch <david.winchurch@iterativeit.com>
2016-03-24 13:57:25 +0000

    US923: Refactoring tests

--- src/SFA.Apprenticeships.Infrastructure.UnitTests/Raa/Extensions/VacancyExtensions/GetCategoryTests.cs
+++ src/SFA.Apprenticeships.Infrastructure.UnitTests/Raa/Extensions/VacancyExtensions/GetCategoryTests.cs
@@ -1,18 +1,15 @@
-﻿namespace SFA.Apprenticeships.Infrastructure.UnitTests.Raa.Extensions
+﻿namespace SFA.Apprenticeships.Infrastructure.UnitTests.Raa.Extensions.VacancyExtensions
 {
-    using System;
     using System.Collections.Generic;
     using Domain.Entities.Raa.Vacancies;
     using Domain.Entities.ReferenceData;
-    using Domain.Entities.Vacancies;
     using FluentAssertions;
     using Infrastructure.Raa.Extensions;
     using NUnit.Framework;
     using Ploeh.AutoFixture;
-    using VacancyType = Domain.Entities.Raa.Vacancies.VacancyType;
 
     [TestFixture]
-    public class VacancyExtensionsTests
+    public class GetCategoryTests
     {
         private List<Category> _categories;
 
@@ -57,34 +54,6 @@
                     }
                 }
             };
-        }
-
-        [TestCase(VacancyStatus.Unknown, VacancyStatuses.Unknown)]
-        [TestCase(VacancyStatus.Draft, VacancyStatuses.Unavailable)]
-        [TestCase(VacancyStatus.Submitted, VacancyStatuses.Unavailable)]
-        [TestCase(VacancyStatus.Live, VacancyStatuses.Live)]
-        [TestCase(VacancyStatus.ReservedForQA, VacancyStatuses.Unavailable)]
-        [TestCase(VacancyStatus.Referred, VacancyStatuses.Unavailable)]
-        [TestCase(VacancyStatus.Closed, VacancyStatuses.Expired)]
-        public void ShouldMapVacancyStatus(VacancyStatus fromVacancyStatus, VacancyStatuses expectedVacancyStatus)
-        {
-            // Act.
-            var actualVacancyStatus = fromVacancyStatus.GetVacancyStatuses();
-
-            // Assert.
-            actualVacancyStatus.Should().Be(expectedVacancyStatus);
-        }
-
-        [Test]
-        public void ShouldThrowIfUnhandledVacancyStatus()
-        {
-            const VacancyStatus unhandledVacancyStatus = VacancyStatus.PostedInError;
-
-            // Act.
-            Action action = () => unhandledVacancyStatus.GetVacancyStatuses();
-
-            // Assert.
-            action.ShouldThrow<ArgumentException>();
         }
 
         [TestCase(VacancyType.Apprenticeship, "101", "FW.101")]
