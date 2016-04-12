682423af40d4a3b30d207022d6c3927ea50cf174
Vicenç García Altés <vincentredrum@gmail.com>
2016-04-12 09:09:31 +0100

    Changed error code

--- src/SFA.Apprenticeships.Infrastructure.EmployerDataService/ErrorCodes.cs
+++ src/SFA.Apprenticeships.Infrastructure.EmployerDataService/ErrorCodes.cs
@@ -3,5 +3,6 @@
     public static class ErrorCodes
     {
         public const string GetByReferenceNumberFailed = "EmployerDataService.GetByReferenceNumberFailed";
+        public const string FindFailed = "EmployerDataService.FindFailed";
     }
 }
