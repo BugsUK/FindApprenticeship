d44ba8b9c6353dc443079a24f16c4a8cdd86907e
alangorton <alan.gorton@valtech.co.uk>
2016-04-13 15:58:12 +0100

    us1259: fix build error.

--- src/SFA.Apprenticeships.Domain.Entities/Vacancies/Apprenticeships/ApprenticeshipVacancyDetail.cs
+++ src/SFA.Apprenticeships.Domain.Entities/Vacancies/Apprenticeships/ApprenticeshipVacancyDetail.cs
@@ -1,11 +1,7 @@
 ﻿namespace SFA.Apprenticeships.Domain.Entities.Vacancies.Apprenticeships
 {
-    using System;
-
     public class ApprenticeshipVacancyDetail : VacancyDetail
     {
-        #region Vacancy
-
         public ApprenticeshipLevel ApprenticeshipLevel { get; set; }
 
         public ApprenticeshipLocationType VacancyLocationType { get; set; }
