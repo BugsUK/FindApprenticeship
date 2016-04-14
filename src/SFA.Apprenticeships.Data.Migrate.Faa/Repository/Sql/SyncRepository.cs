0a685d1030bd844be6273da65830e09737cc5483
David Winchurch <david.winchurch@iterativeit.com>
2016-04-14 13:56:31 +0100

    US1217: Added missing files

---
+++ src/SFA.Apprenticeships.Data.Migrate.Faa/Repository/Sql/SyncRepository.cs
@@ -0,0 +1,36 @@
+﻿namespace SFA.Apprenticeships.Data.Migrate.Faa.Repository.Sql
+{
+    using System;
+    using System.Linq;
+    using Infrastructure.Repositories.Sql.Common;
+
+    public class SyncRepository
+    {
+        private readonly IGetOpenConnection _getOpenConnection;
+
+        public SyncRepository(IGetOpenConnection getOpenConnection)
+        {
+            _getOpenConnection = getOpenConnection;
+        }
+
+        public DateTime? GetApplicationLastCreatedDate()
+        {
+            return _getOpenConnection.Query<DateTime?>("SELECT ApplicationLastCreatedDate FROM Sync.SyncParams").SingleOrDefault();
+        }
+
+        public DateTime? GetApplicationLastUpdatedDate()
+        {
+            return _getOpenConnection.Query<DateTime?>("SELECT ApplicationLastUpdatedDate FROM Sync.SyncParams").SingleOrDefault();
+        }
+
+        public void SetApplicationLastCreatedDate(DateTime applicationLastCreatedDate)
+        {
+            _getOpenConnection.MutatingQuery<int>($"UPDATE Sync.SyncParams SET ApplicationLastCreatedDate = @applicationLastCreatedDate", new { applicationLastCreatedDate });
+        }
+
+        public void SetApplicationLastUpdatedDate(DateTime applicationLastUpdatedDate)
+        {
+            _getOpenConnection.MutatingQuery<int>($"UPDATE Sync.SyncParams SET ApplicationLastUpdatedDate = @applicationLastUpdatedDate", new { applicationLastUpdatedDate });
+        }
+    }
+}
