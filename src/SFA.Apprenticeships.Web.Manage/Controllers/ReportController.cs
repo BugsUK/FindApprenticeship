4b1d1a22a724bd72f68afcded6fc8b59041c0c4b
valtechovais <ovais.oozeer@valtech.co.uk>
2016-04-21 11:03:15 +0100

    WIP, report menu

--- src/SFA.Apprenticeships.Web.Manage/Controllers/ReportController.cs
+++ src/SFA.Apprenticeships.Web.Manage/Controllers/ReportController.cs
@@ -1,16 +1,13 @@
 ﻿namespace SFA.Apprenticeships.Web.Manage.Controllers
 {
     using System;
-    using System.Net;
-    using System.Security.Principal;
     using System.Web.Mvc;
-    using System.Web.UI.WebControls;
     using Attributes;
     using Common.Configuration;
     using Domain.Entities.Raa;
     using Mediators.Reporting;
-    using Microsoft.Reporting.WebForms;
     using SFA.Infrastructure.Interfaces;
+    using ViewModels;
 
     [AuthorizeUser(Roles = Roles.Raa)]
     public class ReportController : ManagementControllerBase
@@ -26,6 +23,13 @@
 
         [HttpGet]
         [AuthorizeUser(Roles = Roles.Raa)]
+        public ActionResult VacanciesListCsv()
+        {
+            return View(new ReportVacanciesParameters());
+        }
+
+        [HttpPost]
+        [AuthorizeUser(Roles = Roles.Raa)]
         public FileContentResult VacanciesListCsv(DateTime fromDate, DateTime toDate)
         {
             var csvBytes = _reportingMediator.ReportVacanciesList(fromDate, toDate);
@@ -34,58 +38,63 @@
 
         public ActionResult Index()
         {
-            var reportViewer = new ReportViewer();
-            reportViewer.ProcessingMode = ProcessingMode.Remote;
-
-            reportViewer.ServerReport.ReportPath = "/NAVMS/VacanciesCSV";
-            reportViewer.ServerReport.ReportServerUrl = new Uri(_reportServerConfiguration.ReportServerUrl);
-            reportViewer.ServerReport.ReportServerCredentials = new ReportServerCredentials(_reportServerConfiguration);
-
-            reportViewer.Width = Unit.Pixel(900);
-            reportViewer.Height = Unit.Pixel(1200);
-
-            ViewBag.ReportViewer = reportViewer;
-
-            return View();
+            return View(new ReportMenu());
         }
 
-        public class ReportServerCredentials : IReportServerCredentials
-        {
-            private readonly ReportServerConfiguration _reportServerConfiguration;
+        //public ActionResult Index()
+        //{
+        //    var reportViewer = new ReportViewer();
+        //    reportViewer.ProcessingMode = ProcessingMode.Remote;
 
-            public ReportServerCredentials(ReportServerConfiguration reportServerConfiguration)
-            {
-                _reportServerConfiguration = reportServerConfiguration;
-            }
+        //    reportViewer.ServerReport.ReportPath = "/NAVMS/VacanciesCSV";
+        //    reportViewer.ServerReport.ReportServerUrl = new Uri(_reportServerConfiguration.ReportServerUrl);
+        //    reportViewer.ServerReport.ReportServerCredentials = new ReportServerCredentials(_reportServerConfiguration);
 
-            public bool GetFormsCredentials(out Cookie authCookie, out string userName, out string password, out string authority)
-            {
-                authCookie = null;
-                userName = null;
-                password = null;
-                authority = null;
+        //    reportViewer.Width = Unit.Pixel(900);
+        //    reportViewer.Height = Unit.Pixel(1200);
 
-                // Not using form credentials
-                return false;
-            }
+        //    ViewBag.ReportViewer = reportViewer;
 
-            public WindowsIdentity ImpersonationUser
-            {
-                get
-                {
-                    // Use the default Windows user.  Credentials will be
-                    // provided by the NetworkCredentials property.
-                    return null;
-                }
-            }
+        //    return View();
+        //}
 
-            public ICredentials NetworkCredentials
-            {
-                get
-                {
-                    return new NetworkCredential(_reportServerConfiguration.NetworkUsername, _reportServerConfiguration.NetworkPassword, _reportServerConfiguration.NetworkDomain);
-                }
-            }
-        }
+        //public class ReportServerCredentials : IReportServerCredentials
+        //{
+        //    private readonly ReportServerConfiguration _reportServerConfiguration;
+
+        //    public ReportServerCredentials(ReportServerConfiguration reportServerConfiguration)
+        //    {
+        //        _reportServerConfiguration = reportServerConfiguration;
+        //    }
+
+        //    public bool GetFormsCredentials(out Cookie authCookie, out string userName, out string password, out string authority)
+        //    {
+        //        authCookie = null;
+        //        userName = null;
+        //        password = null;
+        //        authority = null;
+
+        //        // Not using form credentials
+        //        return false;
+        //    }
+
+        //    public WindowsIdentity ImpersonationUser
+        //    {
+        //        get
+        //        {
+        //            // Use the default Windows user.  Credentials will be
+        //            // provided by the NetworkCredentials property.
+        //            return null;
+        //        }
+        //    }
+
+        //    public ICredentials NetworkCredentials
+        //    {
+        //        get
+        //        {
+        //            return new NetworkCredential(_reportServerConfiguration.NetworkUsername, _reportServerConfiguration.NetworkPassword, _reportServerConfiguration.NetworkDomain);
+        //        }
+        //    }
+        //}
     }
 }
---
+++ src/SFA.Apprenticeships.Web.Manage/ViewModels/ReportVacanciesParameters.cs
@@ -0,0 +1,10 @@
+﻿namespace SFA.Apprenticeships.Web.Manage.ViewModels
+{
+    using System;
+
+    public class ReportVacanciesParameters
+    {
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+    }
+}
