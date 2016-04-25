354dc01af54be7dbb2e96f1b382f1a9c03c1df42
Vicenç García Altés <vincentredrum@gmail.com>
2016-04-25 14:54:46 +0100

    Added number of applications to the query (not filtering by any status)

--- src/SFA.Apprenticeships.Data.AvmsPlus/dbo/Stored Procedures/ReportGetVacancyExtensions.sql
+++ src/SFA.Apprenticeships.Data.AvmsPlus/dbo/Stored Procedures/ReportGetVacancyExtensions.sql
@@ -8,7 +8,7 @@ AS
 
 	declare @VacanciesWithExtensions table (VacancyId int,VacancyOwnerRelationshipId int, ApplicationClosingDate datetime null, ContractOwnerId int, VacancyStatus int)
 	declare @TempResults table (VacancyId int, VacancyStatus int, VacancyOwnerRelationshipId int, EmployerId int, EmployerName nvarchar(255), ProviderSiteId int,
-		ContractOwnerId int, OriginalPostingDate datetime, CurrentClosingDate datetime, NumberOfExtensions int)
+		ContractOwnerId int, OriginalPostingDate datetime, CurrentClosingDate datetime, NumberOfExtensions int, NumberOfApplications int)
 	declare @ProvidersToStudy table (ProviderId int)
 
 	IF (@providerToStudy is null ) BEGIN
@@ -45,10 +45,14 @@ AS
 		from VacancyHistory as vh
 		where vh.VacancyHistoryEventTypeId = 1 and vh.VacancyHistoryEventSubTypeId = 2
 		GROUP BY VacancyId
+	), cteNumberOfApplications(VacancyId, NumberOfApplications) AS (
+		SELECT VacancyId, count(VacancyId)
+		from Application as app
+		GROUP BY VacancyId
 	)
 
 	INSERT INTO @TempResults (VacancyId, VacancyStatus, VacancyOwnerRelationshipId, EmployerId, EmployerName, ProviderSiteId, ContractOwnerId, OriginalPostingDate, 
-		CurrentClosingDate, NumberOfExtensions)
+		CurrentClosingDate, NumberOfExtensions, NumberOfApplications)
 		SELECT 
 			vwe.VacancyId,
 			vwe.VacancyStatus,
@@ -59,13 +63,15 @@ AS
 			vwe.ContractOwnerId,
 			cteOriginalPostingDate.HistoryDate as OriginalPostingDate,
 			vwe.ApplicationClosingDate as CurrentClosingDate,
-			cteNumberOfVacancyExtensions.NumberOfExtensions as NumberOfExtensions
+			cteNumberOfVacancyExtensions.NumberOfExtensions as NumberOfExtensions,
+			cteNumberOfApplications.NumberOfApplications as NumberOfApplications
 		FROM @VacanciesWithExtensions as vwe
 		INNER JOIN VacancyOwnerRelationship as vor ON vwe.VacancyOwnerRelationshipId=vor.VacancyOwnerRelationshipId
 		INNER JOIN Employer as e ON vor.EmployerId=e.EmployerId
 		INNER JOIN ProviderSite as ps ON ps.ProviderSiteID=vor.ProviderSiteID
 		INNER JOIN cteOriginalPostingDate ON cteOriginalPostingDate.VacancyId = vwe.VacancyId
 		INNER JOIN cteNumberOfVacancyExtensions ON cteNumberOfVacancyExtensions.VacancyId = vwe.VacancyId
+		LEFT JOIN cteNumberOfApplications ON cteNumberOfApplications.VacancyId = vwe.VacancyId
 		WHERE cteOriginalPostingDate.HistoryDate >= @startReportDateTime AND 
 			  cteOriginalPostingDate.HistoryDate <= @endReportDateTime 
 		ORDER BY VacancyId
@@ -81,16 +87,18 @@ AS
 		WHERE RowNumber = 1
 	)
 
-	SELECT tmp.*, p.FullName as ProviderName
-		FROM @TempResults as tmp
-		INNER JOIN cteProviderId ON cteProviderId.ProviderSiteID = tmp.ProviderSiteID
-		INNER JOIN dbo.Provider as p ON cteProviderId.ProviderID = p.ProviderID
-		WHERE tmp.ContractOwnerId is null AND 
-			  p.ProviderID IN ( SELECT ProviderId FROM @ProvidersToStudy)
-	UNION 
-		SELECT tmp.*, p.FullName as ProviderName FROM @TempResults as tmp
-		INNER JOIN dbo.Provider as p ON tmp.ContractOwnerId = p.ProviderID
-		WHERE tmp.ContractOwnerId is not null AND 
-			  p.ProviderID IN ( SELECT ProviderId FROM @ProvidersToStudy)
+	SELECT VacancyId, VacancyStatus, VacancyOwnerRelationshipId, EmployerId, EmployerName, tmp.ProviderSiteId, ContractOwnerId, OriginalPostingDate, 
+	CurrentClosingDate, NumberOfExtensions, ISNULL(NumberOfApplications, 0) as NumberOfApplications, p.FullName as ProviderName
+	FROM @TempResults as tmp
+	INNER JOIN cteProviderId ON cteProviderId.ProviderSiteID = tmp.ProviderSiteID
+	INNER JOIN dbo.Provider as p ON cteProviderId.ProviderID = p.ProviderID
+	WHERE tmp.ContractOwnerId is null AND 
+		  p.ProviderID IN ( SELECT ProviderId FROM @ProvidersToStudy)
+UNION 
+	SELECT VacancyId, VacancyStatus, VacancyOwnerRelationshipId, EmployerId, EmployerName, tmp.ProviderSiteId, ContractOwnerId, OriginalPostingDate, 
+	CurrentClosingDate, NumberOfExtensions, ISNULL(NumberOfApplications, 0) as NumberOfApplications, p.FullName as ProviderName FROM @TempResults as tmp
+	INNER JOIN dbo.Provider as p ON tmp.ContractOwnerId = p.ProviderID
+	WHERE tmp.ContractOwnerId is not null AND 
+		  p.ProviderID IN ( SELECT ProviderId FROM @ProvidersToStudy)
 
 	END
