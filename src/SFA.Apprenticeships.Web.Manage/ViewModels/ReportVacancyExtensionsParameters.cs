f2c6b9a89f2d8e18ccda4084886cbf1efeaed829
valtechovais <ovais.oozeer@valtech.co.uk>
2016-04-26 11:17:20 +0100

    vacancy extensions parameters

--- src/SFA.Apprenticeships.Web.Manage/ViewModels/ReportVacancyExtensionsParameters.cs
+++ src/SFA.Apprenticeships.Web.Manage/ViewModels/ReportVacancyExtensionsParameters.cs
@@ -5,6 +5,7 @@ using System.Web;
 
 namespace SFA.Apprenticeships.Web.Manage.ViewModels
 {
+    using System.Web.UI.WebControls;
     using Domain.Entities.Raa.Vacancies;
 
     public class ReportVacancyExtensionsParameters
@@ -13,7 +14,20 @@ namespace SFA.Apprenticeships.Web.Manage.ViewModels
 
         public DateTime ToDate { get; set; }
 
-        public VacancyStatus?  Status { get; set; }
+        public string Status { get; set; }
+
+        public List<ListItem> VacancyStatuses
+        {
+            get
+            {
+                return new List<ListItem>()
+                {
+                    new ListItem("All", "All"),
+                    new ListItem(VacancyStatus.Live.ToString(), ((int)VacancyStatus.Live).ToString()),
+                    new ListItem(VacancyStatus.Closed.ToString(), ((int)VacancyStatus.Closed).ToString())
+                };
+            }
+        }
 
         public int? UKPRN { get; set; }
     }
