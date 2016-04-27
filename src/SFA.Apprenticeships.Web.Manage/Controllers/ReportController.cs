8f527075bca62434d12340f9d5da54b06eec255c
David Winchurch <david.winchurch@iterativeit.com>
2016-04-27 16:03:43 +0100

    US1473: Unsuccessful Candidates UI download link. Could be improved

--- src/SFA.Apprenticeships.Web.Manage/Controllers/ReportController.cs
+++ src/SFA.Apprenticeships.Web.Manage/Controllers/ReportController.cs
@@ -2,6 +2,7 @@
 {
     using System.Web.Mvc;
     using Attributes;
+    using Common.Attributes;
     using Common.Validators.Extensions;
     using Domain.Entities.Raa;
     using Mediators.Reporting;
@@ -32,7 +33,8 @@
             switch (validationResponse.Code)
             {
                 case ReportingMediatorCodes.ReportCodes.Ok:
-                    var response = _reportingMediator.GetVacanciesListReportBytes(parameters);
+                    return View(validationResponse.ViewModel);
+                    /*var response = _reportingMediator.GetVacanciesListReportBytes(parameters);
                     switch (response.Code)
                     {
                         case ReportingMediatorCodes.ReportCodes.Ok:
@@ -42,7 +44,7 @@
                             ModelState.Clear();
                             response.ValidationResult.AddToModelStateWithSeverity(ModelState, string.Empty);
                             return View(parameters);
-                    }
+                    }*/
                 case ReportingMediatorCodes.ReportCodes.ValidationError:
                 default:
                     ModelState.Clear();
@@ -67,6 +69,9 @@
         public ActionResult SuccessfulCandidatesCsv(ReportSuccessfulCandidatesParameters parameters)
         {
             var validationResponse = _reportingMediator.Validate(parameters);
+            var newParameterSet = _reportingMediator.GetUnsuccessfulCandidatesReportParams();
+            validationResponse.ViewModel.ManagedByList = newParameterSet.ViewModel.ManagedByList;
+            validationResponse.ViewModel.RegionList = newParameterSet.ViewModel.RegionList;
             switch (validationResponse.Code)
             {
                 case ReportingMediatorCodes.ReportCodes.Ok:
@@ -85,9 +90,6 @@
                 default:
                     ModelState.Clear();
                     validationResponse.ValidationResult.AddToModelStateWithSeverity(ModelState, string.Empty);
-                    var newParameterSet = _reportingMediator.GetUnsuccessfulCandidatesReportParams();
-                    validationResponse.ViewModel.ManagedByList = newParameterSet.ViewModel.ManagedByList;
-                    validationResponse.ViewModel.RegionList = newParameterSet.ViewModel.RegionList;
                     return View(validationResponse.ViewModel);
             }
         }
@@ -103,34 +105,34 @@
             return View(response.ViewModel);
         }
 
+        [MultipleFormActionsButton(SubmitButtonActionName = "UnsuccessfulCandidatesCsv")]
         [HttpPost]
         [AuthorizeUser(Roles = Roles.Raa)]
-        public ActionResult UnsuccessfulCandidatesCsv(ReportUnsuccessfulCandidatesParameters parameters)
+        public ActionResult ValidateUnsuccessfulCandidatesCsv(ReportUnsuccessfulCandidatesParameters parameters)
         {
             var validationResponse = _reportingMediator.Validate(parameters);
+            var newParameterSet = _reportingMediator.GetUnsuccessfulCandidatesReportParams();
+            validationResponse.ViewModel.ManagedByList = newParameterSet.ViewModel.ManagedByList;
+            validationResponse.ViewModel.RegionList = newParameterSet.ViewModel.RegionList;
             switch (validationResponse.Code)
             {
                 case ReportingMediatorCodes.ReportCodes.Ok:
-                    var response = _reportingMediator.GetUnsuccessfulCandidatesReportBytes(parameters);
-                    switch (response.Code)
-                    {
-                        case ReportingMediatorCodes.ReportCodes.Ok:
-                            return File(response.ViewModel, "text/csv", "UnsuccessfulCandidatesCsv.csv");
-                        case ReportingMediatorCodes.ReportCodes.Error:
-                        default:
-                            ModelState.Clear();
-                            response.ValidationResult.AddToModelStateWithSeverity(ModelState, string.Empty);
-                            return View(parameters);
-                    }
+                    return View(validationResponse.ViewModel);
                 case ReportingMediatorCodes.ReportCodes.ValidationError:
                 default:
                     ModelState.Clear();
                     validationResponse.ValidationResult.AddToModelStateWithSeverity(ModelState, string.Empty);
-                    var newParameterSet = _reportingMediator.GetUnsuccessfulCandidatesReportParams();
-                    validationResponse.ViewModel.ManagedByList = newParameterSet.ViewModel.ManagedByList;
-                    validationResponse.ViewModel.RegionList = newParameterSet.ViewModel.RegionList;
-                    return View(validationResponse.ViewModel);
+                    return View("UnsuccessfulCandidatesCsv", validationResponse.ViewModel);
             }
+        }
+
+        [MultipleFormActionsButton(SubmitButtonActionName = "UnsuccessfulCandidatesCsv")]
+        [HttpPost]
+        [AuthorizeUser(Roles = Roles.Raa)]
+        public ActionResult DownloadUnsuccessfulCandidatesCsv(ReportUnsuccessfulCandidatesParameters parameters)
+        {
+            var response = _reportingMediator.GetUnsuccessfulCandidatesReportBytes(parameters);
+            return File(response.ViewModel, "text/csv", "UnsuccessfulCandidatesCsv.csv");
         }
 
         [HttpGet]
