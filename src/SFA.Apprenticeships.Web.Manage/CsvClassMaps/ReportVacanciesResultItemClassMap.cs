6cc8239db9880d406f8509c3854062bd36e58e45
valtechovais <ovais.oozeer@valtech.co.uk>
2016-04-29 15:36:06 +0100

    vacancies list csv mapper

--- src/SFA.Apprenticeships.Web.Manage/CsvClassMaps/ReportVacanciesResultItemClassMap.cs
+++ src/SFA.Apprenticeships.Web.Manage/CsvClassMaps/ReportVacanciesResultItemClassMap.cs
@@ -5,6 +5,24 @@
 
     public class ReportVacanciesResultItemClassMap : CsvClassMap<ReportVacanciesResultItem>
     {
-         
+        public ReportVacanciesResultItemClassMap()
+        {
+            Map(m => m.VacancyTitle).Name("Vacancy_Title");
+            Map(m => m.VacancyType).Name("Vacancy_Type");
+            Map(m => m.Reference).Name("Reference");
+            Map(m => m.EmployerName).Name("Employer_Name");
+            Map(m => m.EmployerAnonymousName).Name("Employer_Anonymous_Name");
+            Map(m => m.IsEmployerAnonymous).Name("Anonymous_Employer");
+            Map(m => m.Postcode).Name("Postcode");
+            Map(m => m.Sector).Name("Sector");
+            Map(m => m.Framework).Name("Framework");
+            Map(m => m.LearningProvider).Name("Learning_Provider");
+            Map(m => m.NumberOfPositions).Name("Number_of_Vacancies");
+            Map(m => m.NoOfPositionsAvailable).Name("Number_of_Vacancies_Available");
+            Map(m => m.DatePosted).Name("Date_Posted");
+            Map(m => m.ClosingDate).Name("Closing_Date");
+            Map(m => m.NoOfApplications).Name("No_Of_Applications");
+            Map(m => m.Status).Name("Status");
+        }
     }
 }
