7d55f2da40ee2ce2a3826163ae0196b3f2c7b238
David Winchurch <david.winchurch@iterativeit.com>
2016-04-29 15:00:21 +0100

    US1472: Populate ReportAgeRange table

---
+++ src/SFA.Apprenticeships.Data.AvmsPlus/Scripts/Data/dbo.ReportAgeRange.Upsert.sql
@@ -0,0 +1,20 @@
+﻿MERGE INTO [dbo].[ReportAgeRanges] AS Target 
+USING (VALUES 
+	(1, N'Up to 16', 0, 15),
+	(2, N'16 - 18', 16, 18),
+	(3, N'Up to 20', 0, 19),
+	(4, N'19 - 24', 19, 24),
+	(5, N'25 +', 25, 250)
+) 
+AS Source (ReportAgeRangeID, ReportAgeRangeLabel, MinYears, MaxYears) 
+ON Target.ReportAgeRangeID = Source.ReportAgeRangeID 
+-- update matched rows 
+WHEN MATCHED THEN 
+UPDATE SET ReportAgeRangeLabel = Source.ReportAgeRangeLabel, MinYears = Source.MinYears, MaxYears = Source.MaxYears
+-- insert new rows 
+WHEN NOT MATCHED BY TARGET THEN 
+INSERT (ReportAgeRangeID, ReportAgeRangeLabel, MinYears, MaxYears) 
+VALUES (ReportAgeRangeID, ReportAgeRangeLabel, MinYears, MaxYears) 
+-- delete rows that are in the target but not the source 
+WHEN NOT MATCHED BY SOURCE THEN 
+DELETE;
