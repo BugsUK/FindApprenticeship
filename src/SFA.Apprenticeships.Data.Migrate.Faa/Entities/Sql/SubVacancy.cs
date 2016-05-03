f4eebaa6078f219432371a33f761f52a817b2099
David Winchurch <david.winchurch@iterativeit.com>
2016-05-03 08:19:54 +0100

    US1472: Added SubVacancy classes

---
+++ src/SFA.Apprenticeships.Data.Migrate.Faa/Entities/Sql/SubVacancy.cs
@@ -0,0 +1,13 @@
+﻿namespace SFA.Apprenticeships.Data.Migrate.Faa.Entities.Sql
+{
+    using System;
+
+    public class SubVacancy
+    {
+        public int SubVacancyId { get; set; }
+        public int VacancyId { get; set; }
+        public int? AllocatedApplicationId { get; set; }
+        public DateTime? StartDate { get; set; }
+        public string ILRNumber { get; set; }
+    }
+}
---
+++ src/SFA.Apprenticeships.Data.Migrate.Faa/Repository/Sql/SubVacancyRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace SFA.Apprenticeships.Data.Migrate.Faa.Repository.Sql
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Entities.Sql;
+    using Infrastructure.Repositories.Sql.Common;
+
+    public class SubVacancyRepository
+    {
+        private readonly IGetOpenConnection _getOpenConnection;
+
+        public SubVacancyRepository(IGetOpenConnection getOpenConnection)
+        {
+            _getOpenConnection = getOpenConnection;
+        }
+
+        public IDictionary<int, ApplicationSummary> GetApplicationSummariesByIds(IEnumerable<int> applicationIds)
+        {
+            return _getOpenConnection.Query<ApplicationSummary>("SELECT ApplicationId, OutcomeReasonOther FROM Application WHERE ApplicationId in @applicationIds", new { applicationIds }).ToDictionary(a => a.ApplicationId, a => a);
+        }
+    }
+}
