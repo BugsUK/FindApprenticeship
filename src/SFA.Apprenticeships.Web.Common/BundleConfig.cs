673845b685ca9968e41505c58286685b5fd0b0c7
Shoma Gujjar <shoma.gujjar@valtech.co.uk>
2016-05-11 13:40:19 +0100

    Revert the changes made to exclude cdn.

--- src/SFA.Apprenticeships.Web.Common/BundleConfig.cs
+++ src/SFA.Apprenticeships.Web.Common/BundleConfig.cs
@@ -6,19 +6,23 @@
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            bundles.Add(new ScriptBundle("~/bundles/jquery",
+                "//cdnjs.cloudflare.com/ajax/libs/jquery/1.11.1/jquery.min.js").Include(
                     "~/Content/_assets/js/vendor/jquery-1.11.1.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/fastclick").Include(
+            bundles.Add(new ScriptBundle("~/bundles/fastclick",
+                "//cdnjs.cloudflare.com/ajax/libs/fastclick/1.0.6/fastclick.min.js").Include(
                     "~/Content/_assets/js/vendor/fastclick-1.0.6.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/underscore").Include(
+            bundles.Add(new ScriptBundle("~/bundles/underscore",
+                "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.7.0/underscore-min.js").Include(
                     "~/Content/_assets/js/vendor/underscore-1.7.0.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/joyride").Include(
                 "~/Content/_assets/js/vendor/jquery.joyride.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/cookie").Include(
+            bundles.Add(new ScriptBundle("~/bundles/cookie",
+                "//cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.js").Include(
                     "~/Content/_assets/js/vendor/jquery.cookie.js"));
 
             // TODO: split in different bundles?
