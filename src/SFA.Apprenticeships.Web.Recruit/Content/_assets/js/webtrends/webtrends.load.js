d0c6a8ca7614adc7548fedbbbe3f5403c1d372dc
Shoma Gujjar <shoma.gujjar@valtech.co.uk>
2016-05-19 11:11:57 +0100

    Webtrends: Base Tag Implementation.

---
+++ src/SFA.Apprenticeships.Web.Recruit/Content/_assets/js/webtrends/webtrends.load.js
@@ -0,0 +1,24 @@
+// WebTrends SmartSource Data Collector Tag v10.4.23
+// Copyright (c) 2016 Webtrends Inc.  All rights reserved.
+// Tag Builder Version: 4.1.3.5
+// Created: 2016.01.13
+window.webtrendsAsyncInit=function(){
+    var dcs=new Webtrends.dcs().init({
+        dcsid:"dcsshohgd10000knn94fshzyj_2m6c",
+        domain:"stats.matraxis.net",
+        timezone:0,
+        i18n:false,
+        adimpressions:true,
+        adsparam:"WT.ac",
+        offsite:true,
+        download:true,
+        downloadtypes:"xls,doc,pdf,txt,csv,zip,docx,xlsx,rar,gzip",
+        anchor:false,
+        javascript: false,
+        onsitedoms:""
+        }).track();
+};
+(function(){
+    var s = document.createElement("script"); s.async = true; s.src = "/Content/_assets/js/webtrends/webtrends.min.js";
+    var s2=document.getElementsByTagName("script")[0]; s2.parentNode.insertBefore(s,s2);
+}());
