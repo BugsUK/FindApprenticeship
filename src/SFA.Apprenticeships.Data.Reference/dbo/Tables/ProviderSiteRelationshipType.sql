3dd6f18637beafc308dd68d21259bd419f5d7e99
Scott Cowan <info@scottcowan.org>
2016-06-09 11:39:38 +0100

    Removing unused project SFA.Apprenticeships.Data.Reference

--- src/SFA.Apprenticeships.Data.Reference/dbo/Tables/ProviderSiteRelationshipType.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[ProviderSiteRelationshipType] (
-    [ProviderSiteRelationshipTypeID]   INT            NOT NULL,
-    [ProviderSiteRelationshipTypeName] NVARCHAR (100) NOT NULL,
-    CONSTRAINT [PK_ProviderSiteRelationshipType] PRIMARY KEY CLUSTERED ([ProviderSiteRelationshipTypeID] ASC)
-);
-
