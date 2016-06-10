c408e92bd381e25d78e3c5d29dab9dc3fc4ecaa5
Vicenç García Altés <vincentredrum@gmail.com>
2016-06-10 12:51:37 +0100

    Changed IoC in Recruit and manage to use only Raa instances.

---
+++ src/SFA.Apprenticeships.Web.Manage/IoC/VacancySourceRegistry.cs
@@ -0,0 +1,39 @@
+﻿namespace SFA.Apprenticeships.Web.Manage.IoC
+{
+    using Application.Applications;
+    using Application.ReferenceData;
+    using Application.Reporting;
+    using Application.Vacancies;
+    using Application.Vacancy;
+    using Domain.Entities.Vacancies.Apprenticeships;
+    using Domain.Entities.Vacancies.Traineeships;
+    using Infrastructure.Raa;
+    using StructureMap.Configuration.DSL;
+
+    public class VacancySourceRegistry : Registry
+    {
+        public VacancySourceRegistry()
+        {
+            // In Web.Manage we only use Raa implementation (no need to read or write anything from AVMS).
+            // Therefor we can just inject Raa dependencies
+            // TODO: same contents than the same file in Web.Recruit. Consider using a common class? 
+
+            For<IVacancyIndexDataProvider>().Use<VacancyIndexDataProvider>();
+
+            For<IVacancyDataProvider<ApprenticeshipVacancyDetail>>()
+                .Use<ApprenticeshipVacancyDataProvider>();
+
+            For<IVacancyDataProvider<TraineeshipVacancyDetail>>()
+                .Use<TraineeshipVacancyDataProvider>();
+
+            For<ILegacyApplicationStatusesProvider>()
+                .Use<NullApplicationStatusesProvider>();
+
+            For<IReferenceDataProvider>()
+                .Use<ReferenceDataProvider>();
+
+            For<IReportingProvider>()
+                .Use<ReportingProvider>();
+        }
+    }
+}
