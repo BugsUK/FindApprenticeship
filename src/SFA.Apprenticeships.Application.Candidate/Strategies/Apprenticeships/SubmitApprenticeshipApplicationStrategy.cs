d1f640613c6ead32b529066c0d39f16a5dcf14d3
Vicenç García Altés <vincentredrum@gmail.com>
2016-06-23 11:00:05 +0100

    Always publish message when submitting the application

--- src/SFA.Apprenticeships.Application.Candidate/Strategies/Apprenticeships/SubmitApprenticeshipApplicationStrategy.cs
+++ src/SFA.Apprenticeships.Application.Candidate/Strategies/Apprenticeships/SubmitApprenticeshipApplicationStrategy.cs
@@ -1,9 +1,10 @@
 ﻿namespace SFA.Apprenticeships.Application.Candidate.Strategies.Apprenticeships
 {
     using System;
-    using SFA.Infrastructure.Interfaces;
+    using Infrastructure.Interfaces;
     using Domain.Entities.Applications;
     using Domain.Entities.Exceptions;
+    using Domain.Interfaces.Messaging;
     using Domain.Interfaces.Repositories;
     using Interfaces.Communications;
     using MessagingErrorCodes = Interfaces.Messaging.ErrorCodes;
@@ -11,16 +12,21 @@
     public class SubmitApprenticeshipApplicationStrategy : ISubmitApprenticeshipApplicationStrategy
     {
         private readonly ILogService _logger;
+        private readonly IServiceBus _serviceBus;
         private readonly IApprenticeshipApplicationReadRepository _apprenticeshipApplicationReadRepository;
         private readonly IApprenticeshipApplicationWriteRepository _apprenticeshipApplicationWriteRepository;
         private readonly ICommunicationService _communicationService;
 
-        public SubmitApprenticeshipApplicationStrategy(IApprenticeshipApplicationReadRepository apprenticeshipApplicationReadRepository, IApprenticeshipApplicationWriteRepository apprenticeshipApplicationWriteRepository, ICommunicationService communicationService, ILogService logger)
+        public SubmitApprenticeshipApplicationStrategy(
+            IApprenticeshipApplicationReadRepository apprenticeshipApplicationReadRepository,
+            IApprenticeshipApplicationWriteRepository apprenticeshipApplicationWriteRepository,
+            ICommunicationService communicationService, ILogService logger, IServiceBus serviceBus)
         {
             _apprenticeshipApplicationReadRepository = apprenticeshipApplicationReadRepository;
             _apprenticeshipApplicationWriteRepository = apprenticeshipApplicationWriteRepository;
             _communicationService = communicationService;
             _logger = logger;
+            _serviceBus = serviceBus;
         }
 
         public void SubmitApplication(Guid candidateId, int vacancyId)
@@ -33,7 +39,10 @@
             {
                 applicationDetail.SetStateSubmitting();
                 applicationDetail.SetStateSubmitted();
+
                 _apprenticeshipApplicationWriteRepository.Save(applicationDetail);
+
+                PublishMessage(applicationDetail);
                 NotifyCandidate(applicationDetail.CandidateId, applicationDetail.EntityId.ToString());
             }
             catch (Exception ex)
@@ -41,6 +50,28 @@
                 _logger.Debug("SubmitApplicationRequest could not be queued for ApplicationId={0}", applicationDetail.EntityId);
 
                 throw new CustomException("SubmitApplicationRequest could not be queued", ex, MessagingErrorCodes.ApplicationQueuingError);
+            }
+        }
+
+        private void PublishMessage(ApprenticeshipApplicationDetail apprenticeshipApplicationDetail)
+        {
+            try
+            {
+                var message = new SubmitApprenticeshipApplicationRequest
+                {
+                    ApplicationId = apprenticeshipApplicationDetail.EntityId
+                };
+
+                _serviceBus.PublishMessage(message);
+            }
+            catch
+            {
+                // No need to revert to draft in Raa if something fails in publishing the message?
+                // Should we shallow the exception?
+
+                //apprenticeshipApplicationDetail.RevertStateToDraft();
+                //_apprenticeshipApplicationWriteRepository.Save(apprenticeshipApplicationDetail);
+                throw;
             }
         }
 
