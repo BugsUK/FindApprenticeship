ab6d0d441aa8df4eee4155bcf1f071a262c89106
Leo Hendry <leo.hendry@valtech.co.uk>
2016-06-27 16:42:11 +0100

    Split out ICommonApplicationService

---
+++ src/SFA.Apprenticeships.Application.Interfaces/Applications/ICommonApplicationService.cs
@@ -0,0 +1,16 @@
+﻿namespace SFA.Apprenticeships.Application.Interfaces.Applications
+{
+    using System;
+    using System.Collections.Generic;
+
+    public interface ICommonApplicationService
+    {
+        int GetApplicationCount(int vacancyId);
+
+        int GetNewApplicationCount(int vacancyId);
+
+        void UpdateApplicationNotes(Guid applicationId, string notes);
+        
+        int GetNewApplicationsCount(List<int> liveVacancyIds);
+    }
+}
