d7617182f6d394ca3338977ef3a1a33d06e158b5
Vicenç García Altés <vincentredrum@gmail.com>
2016-06-29 13:10:20 +0100

    Refactor

--- src/SFA.Apprenticeships.Web.Recruit.UnitTests/Validators/Providers/ProviderSiteEmployerLinkViewModelValidatorTests.cs
+++ src/SFA.Apprenticeships.Web.Recruit.UnitTests/Validators/Providers/ProviderSiteEmployerLinkViewModelValidatorTests.cs
@@ -56,8 +56,10 @@
         [TestCase("", false)]
         [TestCase("something", true)]
         [TestCase(MassivleyLong, true)]
-        [TestCase(ValidFreeTextLong, true)]
-        [TestCase(InvalidFreeTextLong, false)]
+        [TestCase(Samples.ValidFreeText, true)]
+        [TestCase(Samples.InvalidFreeTextWithInput, false)]
+        [TestCase(Samples.InvalidFreeTextWithObject, false)]
+        [TestCase(Samples.InvalidFreeTextWithScript, false)]
         public void ShouldValidateDescription(
             string description,
             bool expectValid)
@@ -144,21 +146,5 @@
                                              "long for the validator never too long for the validator never too long for the validator " +
                                              "never too long for the validator never too long for the validator never too long for the " +
                                              "validator never too long for the validator";
-
-        private const string ValidFreeTextLong = "<h1>Excellent op<h2>portunities</h2> for Pre School Apprentice&#39;s are " +
-                                                 "available in this established, " +
-                  "friendly nursery.  You will be caring for and meeting <br>the needs of <br><br>children aged between " +
-                  "4 months and 5 years, creating a safe environment and providing stimulating </h1>\r\n<br></br> <br></br>" +
-                  "<br></br>";
-
-        private const string InvalidFreeTextLong = "<h1>Excellent op<h2>portunities</h2> for Pre School Apprentice&#39;s are available in this established, \" " +
-                                                   "+\r\n                  \"friendly nursery.  " +
-                                                   "You will be caring for and meeting <br>the needs of <br><br>children aged between " +
-                                                   "\" +\r\n                  \"4 " +
-                                                   "months and 5 years, creating a safe environment and providing stimulating </h1>\\r\\n<br></br> <br>" +
-                                                   "</br>\" +\r\n                  \"<br>" +
-                                                   "</br><script>alert(\'Hello World\')</script>\"</br>" +
-                                                   "<input>alert(\\\'Hello World\\\')</input>\"\"</br>" +
-                                                   "<object>alert(\\\'Hello World\\\')</object>\"";
     }
 }
