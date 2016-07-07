867522ac9306adf7737788315726caa89d9905f1
Vicenç García Altés <vincentredrum@gmail.com>
2016-07-07 17:28:58 +0100

    publish correctly county and town from javascript selector.

--- src/SFA.Apprenticeships.Web.Candidate/Content/_assets/js/nas/lookupService.js
+++ src/SFA.Apprenticeships.Web.Candidate/Content/_assets/js/nas/lookupService.js
@@ -153,10 +153,11 @@ $(document).ready(function () {
     }
 
     function populateAddress(address) {
+        var county = address.ProvinceName || address.City;
         $('#Address_AddressLine1').val(address.Line1);
         $('#Address_AddressLine2').val(address.Line2);
-        $('#Address_AddressLine3').val(address.Line3);
-        $('#Address_AddressLine4').val(address.City);
+        $('#Address_AddressLine3').val(address.City);
+        $('#Address_AddressLine4').val(county);
         $('#Address_Postcode').val(address.PostalCode);
         $("#Address_Uprn").val(address.DomesticId);
 
