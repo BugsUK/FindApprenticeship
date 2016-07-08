bc653b6a97f9f1de75073c990b0d876a3ab687c3
alangorton <alan.gorton@valtech.co.uk>
2016-07-08 09:23:06 +0100

    DE206: moved code out of Program to accommodate more patches.

--- tools/SetApplicationStatus/SetApplicationStatus.Console/Program.cs
+++ tools/SetApplicationStatus/SetApplicationStatus.Console/Program.cs
@@ -1,8 +1,6 @@
 ﻿namespace SetApplicationStatus.Console
 {
     using System;
-    using System.Linq;
-    using Entities.Sql;
     using NLog;
 
     public class Program
@@ -22,66 +20,29 @@
                 return;
             }
 
-            var ukprn = options.Ukprn;
-            var sqlConnectionString = options.SqlConnectionString;
-            var mongoConnectionString = options.MongoConnectionString;
-            var update = options.Update;
-
             Logger.Info("Started");
 
-            var sqlApplicationRepository = new Repositories.Sql.ApplicationRepository(sqlConnectionString);
-
-            var sourceApplications = sqlApplicationRepository
-                .GetApplicationIdsByUkprn(ukprn)
-                .Where(application => application.Status == ApplicationStatus.InProgress);
-
-            var mongoApplicationRepository = new Repositories.Mongo.ApplicationRepository(mongoConnectionString);
-
-            foreach (var sourceApplication in sourceApplications)
             {
-                Logger.Info($"SOURCE: Application ID: {sourceApplication.ApplicationId}, Status: {sourceApplication.Status}");
+                var patch = new InProgressApplicationPatch(
+                    options.Ukprn,
+                    options.SqlConnectionString,
+                    options.MongoConnectionString,
+                    options.Update);
 
-                var ids = new[]
-                {
-                    sourceApplication.ApplicationId
-                };
-
-                var targetApplications = mongoApplicationRepository.GetApplicationByLegacyIds(ids);
-
-                foreach (var targetApplication in targetApplications)
-                {
-                    Logger.Info($"TARGET: Id: {targetApplication.Id} Legacy Application ID: {targetApplication.LegacyApplicationId}, Status: {targetApplication.Status}");
-
-                    if (targetApplication.Status == Entities.Mongo.ApplicationStatus.InProgress)
-                    {
-                        continue;
-                    }
-
-                    Logger.Warn($"Expected: {Entities.Mongo.ApplicationStatus.InProgress}, Actual: {targetApplication.Status}");
-
-                    if (targetApplication.Status != Entities.Mongo.ApplicationStatus.Submitted)
-                    {
-                        continue;
-                    }
-
-                    if (!update)
-                    {
-                        continue;
-                    }
-
-                    var updated = mongoApplicationRepository.SetApplicationStatus(
-                        targetApplication.Id, Entities.Mongo.ApplicationStatus.InProgress);
-
-                    if (updated)
-                    {
-                        Logger.Info("^Updated");
-                    }
-                    else
-                    {
-                        Logger.Error("^Update failed");
-                    }
-                }
+                patch.Run();
             }
+
+            /*
+            {
+                var patch = new ApplicationStatusPatch(
+                    options.Ukprn,
+                    options.SqlConnectionString,
+                    options.MongoConnectionString,
+                    options.Update);
+
+                patch.Run();
+            }
+            */
 
             Logger.Info("Finished");
         }
