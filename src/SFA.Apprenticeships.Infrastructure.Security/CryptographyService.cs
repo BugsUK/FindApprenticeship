ebc57d97882d0f4ec16bc1df5c8ceaf0c4276ab5
valtechovais <ovais.oozeer@valtech.co.uk>
2016-07-21 13:30:37 +0100

    WIP creating encryption service, unit tests first

---
+++ src/SFA.Apprenticeships.Infrastructure.Security/CryptographyService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFA.Apprenticeships.Infrastructure.Security
+{
+    using Application.Interfaces.Security;
+
+    public class CryptographyService<T> : IEncryptionService<T>, IDecryptionService<T> where T : class
+    {
+        public string Encrypt(T objectToEncrypt)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/SFA.Apprenticeships.Infrastructure.UnitTests/Security/CryptographyServiceTests.cs
@@ -0,0 +1,58 @@
+﻿namespace SFA.Apprenticeships.Infrastructure.UnitTests.Security
+{
+    using System;
+    using FluentAssertions;
+    using Infrastructure.Security;
+    using NUnit.Framework;
+    using Ploeh.AutoFixture;
+
+    [TestFixture]
+    public class CryptographyServiceTests
+    {
+        [Test]
+        public void RequiresObjectToDecrypt()
+        {
+            //Arrange
+            string objectToEncrypt = null;
+            var cryptoService = new CryptographyService<string>();
+
+            //Act && Assert
+            Assert.Throws<ArgumentNullException>(()=> cryptoService.Encrypt(objectToEncrypt));
+        }
+
+        [Test]
+        public void CanEncryptString()
+        {
+            //Arrange
+            var objectToEncrypt = "test string";
+            var cryptoService = new CryptographyService<string>();
+
+            //Act
+            var result = cryptoService.Encrypt(objectToEncrypt);
+
+            //Assert
+            result.Should().Be("");
+        }
+
+        [Test]
+        public void CanEncryptComplexObject()
+        {
+            //Arrange
+            var objectToEncrypt = new Fixture().Create<NestedComplexObject>();
+            var cryptoService = new CryptographyService<NestedComplexObject>();
+
+            //Act
+            var result = cryptoService.Encrypt(objectToEncrypt);
+
+            //Assert
+            result.Should().Be("");
+        }
+    }
+
+    internal sealed class NestedComplexObject
+    {
+        public int Id { get; set; }
+
+        public string PropertyName { get; set; }
+    }
+}
