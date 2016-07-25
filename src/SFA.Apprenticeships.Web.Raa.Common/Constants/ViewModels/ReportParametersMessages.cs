dc7ad90b15c4cbe49c23c6b738af13840d184518
David Winchurch <david.winchurch@iterativeit.com>
2016-07-25 18:17:34 +0100

    US1999: Ensured to and from dates are valid

--- src/SFA.Apprenticeships.Web.Raa.Common/Constants/ViewModels/ReportParametersMessages.cs
+++ src/SFA.Apprenticeships.Web.Raa.Common/Constants/ViewModels/ReportParametersMessages.cs
@@ -6,12 +6,14 @@
         {
             public const string LabelText = "Date from";
             public const string RequiredErrorText = "Date from is required";
+            public const string InvalidDateErrorText = "Date from must be a valid date";
         }
 
         public static class ToDateMessages
         {
             public const string LabelText = "Date to";
             public const string RequiredErrorText = "Date to is required";
+            public const string InvalidDateErrorText = "Date to must be a valid date";
         }
     }
 }
--- src/SFA.Apprenticeships.Web.Raa.Common/Validators/Report/ReportParametersDateRangeValidator.cs
+++ src/SFA.Apprenticeships.Web.Raa.Common/Validators/Report/ReportParametersDateRangeValidator.cs
@@ -21,10 +21,18 @@
             validator.RuleFor(x => x.FromDate)
                 .Must(x => x.HasValue)
                 .WithMessage(ReportParametersMessages.FromDateMessages.RequiredErrorText);
+            validator.RuleFor(x => x.FromDate)
+                .Must(Common.BeValidDate)
+                .WithMessage(ReportParametersMessages.FromDateMessages.InvalidDateErrorText)
+                .When(x => x.FromDate.HasValue);
 
             validator.RuleFor(x => x.ToDate)
                 .Must(x => x.HasValue)
                 .WithMessage(ReportParametersMessages.ToDateMessages.RequiredErrorText);
+            validator.RuleFor(x => x.ToDate)
+                .Must(Common.BeValidDate)
+                .WithMessage(ReportParametersMessages.ToDateMessages.InvalidDateErrorText)
+                .When(x => x.ToDate.HasValue);
         }
     }
 }
