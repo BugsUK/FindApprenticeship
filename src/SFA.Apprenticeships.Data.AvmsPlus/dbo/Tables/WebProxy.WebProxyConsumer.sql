5430f745725635fbb7d29ab6893eb6c2e5a4b60c
Shoma Gujjar <shoma.gujjar@valtech.co.uk>
2016-07-27 14:54:45 +0100

    Include webproxy  table and schema.

---
+++ src/SFA.Apprenticeships.Data.AvmsPlus/dbo/Tables/WebProxy.WebProxyConsumer.sql
@@ -0,0 +1,8 @@
+﻿ CREATE TABLE WebProxy.WebProxyConsumer
+                (
+                    ExternalSystemId                    UNIQUEIDENTIFIER PRIMARY KEY,
+                    WebProxyConsumerId                  INT,
+                    ShortDescription                    VARCHAR(MAX),
+                    FullDescription                     VARCHAR(MAX),
+                    RouteToCompatabilityWebServiceRegex VARCHAR(MAX)
+                );
