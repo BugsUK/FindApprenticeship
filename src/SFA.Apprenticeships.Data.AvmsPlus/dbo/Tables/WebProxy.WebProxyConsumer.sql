3fe3ebdd4c702c956d58b24a0f2548ca0c29ad74
David Winchurch <david.winchurch@iterativeit.com>
2016-08-02 15:30:49 +0100

    Preparing for API launch

--- src/SFA.Apprenticeships.Data.AvmsPlus/dbo/Tables/WebProxy.WebProxyConsumer.sql
+++
@@ -1,8 +0,0 @@
-﻿ CREATE TABLE WebProxy.WebProxyConsumer
-                (
-                    ExternalSystemId                    UNIQUEIDENTIFIER PRIMARY KEY,
-                    WebProxyConsumerId                  INT IDENTITY(1,1),
-                    ShortDescription                    VARCHAR(MAX),
-                    FullDescription                     VARCHAR(MAX),
-                    RouteToCompatabilityWebServiceRegex VARCHAR(MAX)
-                );
