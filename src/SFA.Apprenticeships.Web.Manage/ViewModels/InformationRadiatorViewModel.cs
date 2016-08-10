10fc68a042145daffb6a7c556c4dae4be0e43f1f
David Winchurch <david.winchurch@iterativeit.com>
2016-08-10 14:03:31 +0100

    Added more granular states to vacancies

--- src/SFA.Apprenticeships.Web.Manage/ViewModels/InformationRadiatorViewModel.cs
+++ src/SFA.Apprenticeships.Web.Manage/ViewModels/InformationRadiatorViewModel.cs
@@ -4,8 +4,13 @@
     {
         public int TotalProviders { get; set; }
         public int TotalProviderUserAccounts { get; set; }
-        public int TotalVacanciesSubmittedViaRaa { get; set; }
+        public int TotalVacanciesCreatedViaRaa { get; set; }
+        public int TotalDraftVacanciesCreatedViaRaa { get; set; }
+        public int TotalVacanciesInReviewViaRaa { get; set; }
+        public int TotalVacanciesReferredViaRaa { get; set; }
         public int TotalVacanciesApprovedViaRaa { get; set; }
+        public int TotalVacanciesClosedViaRaa { get; set; }
+        public int TotalVacanciesArchivedViaRaa { get; set; }
         public int TotalApplicationsStartedForRaaVacancies { get; set; }
         public int TotalApplicationsSubmittedForRaaVacancies { get; set; }
         public int TotalUnsuccessfulApplicationsViaRaa { get; set; }
