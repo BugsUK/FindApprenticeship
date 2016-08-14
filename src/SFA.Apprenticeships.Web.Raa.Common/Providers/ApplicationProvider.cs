75aed37a50dc3cbc28cbe163153a695e8d9160d0
David Winchurch <david.winchurch@iterativeit.com>
2016-08-14 09:21:22 +0100

    Fixed ordering of applications

--- src/SFA.Apprenticeships.Web.Raa.Common/Providers/ApplicationProvider.cs
+++ src/SFA.Apprenticeships.Web.Raa.Common/Providers/ApplicationProvider.cs
@@ -142,8 +142,8 @@
             {
                 case VacancyApplicationsSearchViewModel.OrderByFieldLastName:
                     page = order == Order.Descending
-                        ? applications.OrderByDescending(a => a.CandidateDetails.LastName).ThenBy(a => a.DateApplied)
-                        : applications.OrderBy(a => a.CandidateDetails.LastName).ThenBy(a => a.DateApplied);
+                        ? applications.OrderByDescending(a => a.CandidateDetails.LastName).ThenBy(a => a.CandidateDetails.FirstName).ThenByDescending(a => a.DateApplied)
+                        : applications.OrderBy(a => a.CandidateDetails.LastName).ThenBy(a => a.CandidateDetails.FirstName).ThenByDescending(a => a.DateApplied);
                     break;
                 default:
                     page = applications;
