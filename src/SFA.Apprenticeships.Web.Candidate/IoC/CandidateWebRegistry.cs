d78aa939fd181d377909f8f679be2dc345092aa9
David Winchurch <david.winchurch@iterativeit.com>
2016-08-24 11:28:37 +0100

    Displaying legacy AV candidate reference on applications page

--- src/SFA.Apprenticeships.Web.Candidate/IoC/CandidateWebRegistry.cs
+++ src/SFA.Apprenticeships.Web.Candidate/IoC/CandidateWebRegistry.cs
@@ -8,6 +8,7 @@
     using Application.Candidate.Configuration;
     using Application.Candidate.Strategies;
     using Application.Candidate.Strategies.Apprenticeships;
+    using Application.Candidate.Strategies.Candidates;
     using Application.Candidate.Strategies.SavedSearches;
     using Application.Candidate.Strategies.SuggestedVacancies;
     using Application.Candidate.Strategies.Traineeships;
@@ -198,6 +199,7 @@
             For<IApprenticeshipVacancySuggestionsStrategy>().Use<ApprenticeshipVacancySuggestionsStrategy>();
             For<IGetCandidateByUsernameStrategy>().Use<GetCandidateByUsernameStrategy>();
             For<IGetCandidateByIdStrategy>().Use<GetCandidateByIdStrategy>();
+            For<IGetCandidateSummariesStrategy>().Use<GetCandidateSummariesStrategy>();
 
             For<Application.Candidate.Strategies.IUpdateUsernameStrategy>().Use<Application.Candidate.Strategies.UpdateUsernameStrategy>().Ctor<ISaveCandidateStrategy>().Named("QueuedLegacySaveCandidateStrategy").Ctor<ICodeGenerator>().Named(codeGenerator);
             For<Application.UserAccount.Strategies.IUpdateUsernameStrategy>().Use<Application.UserAccount.Strategies.UpdateUsernameStrategy>().Ctor<ICodeGenerator>().Named(codeGenerator);
