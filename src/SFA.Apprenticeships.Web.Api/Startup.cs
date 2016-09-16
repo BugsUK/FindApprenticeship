ba8de4f279e23210da0ae1eedb0cc91531a166eb
Aaron Rhodes <aaroncraigrhodes@googlemail.com>
2016-09-16 11:12:18 +0100

    Added Api project to solution

---
+++ src/SFA.Apprenticeships.Web.Api/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SFA.Apprenticeships.Web.Api.Startup))]
+
+namespace SFA.Apprenticeships.Web.Api
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            
+        }
+    }
+}
