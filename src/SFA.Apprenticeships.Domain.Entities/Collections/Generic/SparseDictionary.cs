8645bc18e2339e8ea16679bb678346c5c2b8c335
David Winchurch <david.winchurch@iterativeit.com>
2016-09-19 16:54:20 +0100

    Application counts from SQL rather than Mongo

---
+++ src/SFA.Apprenticeships.Domain.Entities/Collections/Generic/SparseDictionary.cs
@@ -0,0 +1,79 @@
+﻿namespace SFA.Apprenticeships.Domain.Entities.Collections.Generic
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Dictionary which pretends that keys not present have a default value
+    /// </summary>
+    public class SparseDictionary<K, V> : IReadOnlyDictionary<K, V>
+    {
+        private IReadOnlyDictionary<K, V> _dict;
+
+        private V _default;
+
+        public SparseDictionary(IReadOnlyDictionary<K, V> dict, V defaultvalue)
+        {
+            _dict = dict;
+            _default = defaultvalue;
+        }
+
+        public V this[K key]
+        {
+            get
+            {
+                V result;
+                if (_dict.TryGetValue(key, out result))
+                    return result;
+                else
+                    return _default;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                throw new NotImplementedException("This is a sparse dictionary and so a count isn't very meaningful");
+            }
+        }
+
+        public IEnumerable<K> Keys
+        {
+            get
+            {
+                throw new NotImplementedException("This is a sparse dictionary and logically contains all possible keys");
+            }
+        }
+
+        public IEnumerable<V> Values
+        {
+            get
+            {
+                throw new NotImplementedException("This is a sparse dictionary and logically contains values for all possible keys");
+            }
+        }
+
+        public bool ContainsKey(K key)
+        {
+            return true;
+        }
+
+        public IEnumerator<KeyValuePair<K, V>> GetEnumerator()
+        {
+            throw new NotImplementedException("This is a sparse dictionary and logically contains all possible keys");
+        }
+
+        public bool TryGetValue(K key, out V value)
+        {
+            value = this[key];
+            return true;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException("This is a sparse dictionary and logically contains all possible keys");
+        }
+    }
+}
---
+++ src/SFA.Apprenticeships.Infrastructure.Repositories.Sql/Schemas/dbo/ApplicationStatsRepository.cs
@@ -0,0 +1,64 @@
+﻿namespace SFA.Apprenticeships.Infrastructure.Repositories.Sql.Schemas.dbo
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Common;
+    using Domain.Entities.Applications;
+    using Domain.Entities.Collections.Generic;
+    using Domain.Interfaces.Repositories;
+
+    public class ApplicationStatsRepository : IApprenticeshipApplicationStatsRepository, ITraineeshipApplicationStatsRepository
+    {
+        private readonly IGetOpenConnection _getOpenConnection;
+
+        public ApplicationStatsRepository(IGetOpenConnection getOpenConnection)
+        {
+            _getOpenConnection = getOpenConnection;
+        }
+
+        IReadOnlyDictionary<int, IApplicationCounts> IApprenticeshipApplicationStatsRepository.GetCountsForVacancyIds(IEnumerable<int> vacancyIds)
+        {
+            return GetCounts(vacancyIds);
+        }
+
+        IReadOnlyDictionary<int, IApplicationCounts> ITraineeshipApplicationStatsRepository.GetCountsForVacancyIds(IEnumerable<int> vacancyIds)
+        {
+            return GetCounts(vacancyIds);
+        }
+
+        public IReadOnlyDictionary<int, IApplicationCounts> GetCounts(IEnumerable<int> vacancyIds)
+        {
+            //Implementation is common for SQL as all applications are stored in the same table
+            var counts = new Dictionary<int, IApplicationCounts>();
+            var splitVacancyIdsArray = DbHelpers.SplitIds(vacancyIds);
+            foreach (var ids in splitVacancyIdsArray)
+            {
+                var sqlParams = new
+                {
+                    VacancyIds = ids
+                };
+                var allApplicationCounts = _getOpenConnection.Query<ApplicationCounts>("SELECT VacancyId, Count(*) as AllApplications FROM [Application] WHERE VacancyId IN @VacancyIds AND ApplicationStatusTypeId >= 2 GROUP BY VacancyId", sqlParams);
+                var newApplicationCounts = _getOpenConnection.Query<ApplicationCounts>("SELECT VacancyId, Count(*) as NewApplications FROM [Application] WHERE VacancyId IN @VacancyIds AND ApplicationStatusTypeId = 2 GROUP BY VacancyId", sqlParams);
+
+                foreach (var allApplicationCount in allApplicationCounts)
+                {
+                    counts[allApplicationCount.VacancyId] = allApplicationCount;
+                }
+
+                foreach (var newApplicationCount in newApplicationCounts)
+                {
+                    ((ApplicationCounts)counts[newApplicationCount.VacancyId]).NewApplications = newApplicationCount.NewApplications;
+                }
+            }
+
+            return new SparseDictionary<int, IApplicationCounts>(counts, new ApplicationCounts());
+        }
+
+        private class ApplicationCounts : IApplicationCounts
+        {
+            public int VacancyId { get; set; }
+            public int NewApplications { get; set; }
+            public int AllApplications { get; set; }
+        }
+    }
+}
