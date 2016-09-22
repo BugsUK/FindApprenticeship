e57af502f2075f907d1c2743f391b7fc0af05428
Aaron Rhodes <aaroncraigrhodes@googlemail.com>
2016-09-22 11:50:59 +0100

    Added service status page, view and controller

---
+++ src/SFA.Apprenticeships.Web.Recruit/Controllers/InformationRadiatorController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SFA.Apprenticeships.Web.Recruit.Controllers
+{
+    using Domain.Entities.Raa.Vacancies;
+    using Domain.Interfaces.Repositories;
+    using Domain.Raa.Interfaces.Queries;
+    using Domain.Raa.Interfaces.Repositories;
+    using ViewModels;
+
+    public class InformationRadiatorController : Controller
+    {
+        private IVacancyReadRepository _vacancyRepository;
+
+        public InformationRadiatorController(IVacancyReadRepository vacancyRepository)
+        {
+            _vacancyRepository = vacancyRepository;
+        }
+
+        // GET: InformationRadiator
+        public ActionResult Index()
+        {
+            var viewModel = new InformationRadiatorViewModel();
+
+            int total = 0;
+
+            try
+            {
+                var result = _vacancyRepository.Find(new ApprenticeshipVacancyQuery()
+                {
+                    CurrentPage = 1,
+                    PageSize = 1,
+                    DesiredStatuses = new List<VacancyStatus>() {VacancyStatus.Live}
+                }, out total);
+
+                if (result != null && result.Count >= 1)
+                {
+                    viewModel.DatabaseError = false;
+                    viewModel.DatabaseSuccess = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                viewModel.DatabaseError = true;
+                viewModel.DatabaseException = ex;
+            }
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ src/SFA.Apprenticeships.Web.Recruit/ViewModels/InformationRadiatorViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SFA.Apprenticeships.Web.Recruit.ViewModels
+{
+    public class InformationRadiatorViewModel
+    {
+        public bool DatabaseError { get; set; }
+        public bool DatabaseSuccess { get; set; }
+        public Exception DatabaseException { get; set; }
+    }
+}
