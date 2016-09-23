9a5947c62f63d54ba839b7ed7f60d41344ed9bed
David Winchurch <david.winchurch@iterativeit.com>
2016-09-23 08:58:45 +0100

    Information radiator screen can better cope with database errors

--- src/SFA.Apprenticeships.Web.Manage/Mediators/InformationRadiator/InformationRadiatorMediator.cs
+++ src/SFA.Apprenticeships.Web.Manage/Mediators/InformationRadiator/InformationRadiatorMediator.cs
@@ -1,44 +1,56 @@
 ﻿namespace SFA.Apprenticeships.Web.Manage.Mediators.InformationRadiator
 {
+    using System;
+    using Application.Interfaces;
     using Common.Mediators;
     using Domain.Raa.Interfaces.Reporting;
     using ViewModels;
 
     public class InformationRadiatorMediator : MediatorBase, IInformationRadiatorMediator
     {
+        private readonly ILogService _logService;
         private readonly IReportingRepository _reportingRepo;
 
-        public InformationRadiatorMediator(IReportingRepository reportingRepo)
+        public InformationRadiatorMediator(IReportingRepository reportingRepo, ILogService logService)
         {
             _reportingRepo = reportingRepo;
+            _logService = logService;
         }
 
         public MediatorResponse<InformationRadiatorViewModel> GetInformationRadiatorViewModel()
         {
-            var data = _reportingRepo.GetInformationRadiatorData();
+            var viewModel = new InformationRadiatorViewModel();
 
-            var viewModel = new InformationRadiatorViewModel
+            try
             {
-                TotalProviders = data.TotalProviders,
-                TotalProvidersAskedToOnboard = data.TotalProvidersAskedToOnboard,
-                TotalProvidersForcedToMigrate = data.TotalProvidersForcedToMigrate,
-                TotalProvidersOnboarded = data.TotalProvidersOnboarded,
-                TotalProvidersMigrated = data.TotalProvidersMigrated,
-                TotalProviderUserAccounts = data.TotalProviderUserAccounts,
-                TotalVacanciesCreatedViaRaa = data.TotalVacanciesCreatedViaRaa,
-                TotalDraftVacanciesCreatedViaRaa = data.TotalDraftVacanciesCreatedViaRaa,
-                TotalVacanciesInReviewViaRaa = data.TotalVacanciesInReviewViaRaa,
-                TotalVacanciesReferredViaRaa = data.TotalVacanciesReferredViaRaa,
-                TotalVacanciesApprovedViaRaa = data.TotalVacanciesApprovedViaRaa,
-                TotalVacanciesClosedViaRaa = data.TotalVacanciesClosedViaRaa,
-                TotalVacanciesArchivedViaRaa = data.TotalVacanciesArchivedViaRaa,
-                TotalApplicationsStartedForRaaVacancies = data.TotalApplicationsStartedForRaaVacancies,
-                TotalApplicationsSubmittedForRaaVacancies = data.TotalApplicationsSubmittedForRaaVacancies,
-                TotalUnsuccessfulApplicationsViaRaa = data.TotalUnsuccessfulApplicationsViaRaa,
-                TotalSuccessfulApplicationsViaRaa = data.TotalSuccessfulApplicationsViaRaa
-            };
+                var data = _reportingRepo.GetInformationRadiatorData();
 
-            return GetMediatorResponse(InformationRadiatorMediatorCodes.IndexCodes.Ok, viewModel);
+                viewModel.TotalProviders = data.TotalProviders;
+                viewModel.TotalProvidersAskedToOnboard = data.TotalProvidersAskedToOnboard;
+                viewModel.TotalProvidersForcedToMigrate = data.TotalProvidersForcedToMigrate;
+                viewModel.TotalProvidersOnboarded = data.TotalProvidersOnboarded;
+                viewModel.TotalProvidersMigrated = data.TotalProvidersMigrated;
+                viewModel.TotalProviderUserAccounts = data.TotalProviderUserAccounts;
+                viewModel.TotalVacanciesCreatedViaRaa = data.TotalVacanciesCreatedViaRaa;
+                viewModel.TotalDraftVacanciesCreatedViaRaa = data.TotalDraftVacanciesCreatedViaRaa;
+                viewModel.TotalVacanciesInReviewViaRaa = data.TotalVacanciesInReviewViaRaa;
+                viewModel.TotalVacanciesReferredViaRaa = data.TotalVacanciesReferredViaRaa;
+                viewModel.TotalVacanciesApprovedViaRaa = data.TotalVacanciesApprovedViaRaa;
+                viewModel.TotalVacanciesClosedViaRaa = data.TotalVacanciesClosedViaRaa;
+                viewModel.TotalVacanciesArchivedViaRaa = data.TotalVacanciesArchivedViaRaa;
+                viewModel.TotalApplicationsStartedForRaaVacancies = data.TotalApplicationsStartedForRaaVacancies;
+                viewModel.TotalApplicationsSubmittedForRaaVacancies = data.TotalApplicationsSubmittedForRaaVacancies;
+                viewModel.TotalUnsuccessfulApplicationsViaRaa = data.TotalUnsuccessfulApplicationsViaRaa;
+                viewModel.TotalSuccessfulApplicationsViaRaa = data.TotalSuccessfulApplicationsViaRaa;
+
+                return GetMediatorResponse(InformationRadiatorMediatorCodes.IndexCodes.Ok, viewModel);
+            }
+            catch (Exception ex)
+            {
+                _logService.Warn("Failed to get information radiator data", ex);
+
+                return GetMediatorResponse(InformationRadiatorMediatorCodes.IndexCodes.Error, viewModel);
+            }
         }
     }
 }
