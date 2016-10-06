b5ea87c66457eaa0f660bcac3f7d846214d5cce6
David Winchurch <david.winchurch@iterativeit.com>
2016-10-06 10:40:31 +0100

    Ability to edit statuses for providers and provider sites

---
+++ src/SFA.Apprenticeships.Domain.Entities.Raa/Parties/EmployerTrainingProviderStatuses.cs
@@ -0,0 +1,9 @@
+﻿namespace SFA.Apprenticeships.Domain.Entities.Raa.Parties
+{
+    public enum EmployerTrainingProviderStatuses
+    {
+        Activated = 1,
+        Deleted = 2,
+        Suspended = 3
+    }
+}
