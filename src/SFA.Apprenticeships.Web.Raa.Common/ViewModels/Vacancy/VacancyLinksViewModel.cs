51ba15599b78947c88a7fab06c3f9e7e2c19d99d
Shoma Gujjar <shoma.gujjar@valtech.co.uk>
2016-10-20 16:57:25 +0100

    ra-318: Fix merge issues.

--- src/SFA.Apprenticeships.Web.Raa.Common/ViewModels/Vacancy/VacancyLinksViewModel.cs
+++ src/SFA.Apprenticeships.Web.Raa.Common/ViewModels/Vacancy/VacancyLinksViewModel.cs
@@ -7,19 +7,17 @@
         public const string PartialView = "DisplayTemplates/_VacancyLinks";
 
         public VacancyLinksViewModel(int vacancyReferenceNumber,
-            VacancyStatus status, int totalNumberOfApplications, VacancyType vacancyType, int totalApplicationsRequiringDecision)
+            VacancyStatus status, int totalNumberOfApplications, VacancyType vacancyType)
         {
             VacancyReferenceNumber = vacancyReferenceNumber;
             Status = status;
             TotalNumberOfApplications = totalNumberOfApplications;
             VacancyType = vacancyType;
-            TotalApplicationsRequiringDecision == totalApplicationsRequiringDecision;
         }
 
         public int VacancyReferenceNumber { get; private set; }
         public VacancyStatus Status { get; private set; }
         public int TotalNumberOfApplications { get; private set; }
         public VacancyType VacancyType { get; private set; }
-        public int TotalApplicationsRequiringDecision { get; private set; }
     }
 }
