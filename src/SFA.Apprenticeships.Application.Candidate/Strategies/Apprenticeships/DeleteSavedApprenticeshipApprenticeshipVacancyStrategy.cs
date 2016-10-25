7a812850e54dbddf9e589b742b17aa15d16b7e8a
David Winchurch <david.winchurch@iterativeit.com>
2016-10-25 15:13:13 +0100

    Publishing application create and delete messages

--- src/SFA.Apprenticeships.Application.Candidate/Strategies/Apprenticeships/DeleteSavedApprenticeshipApprenticeshipVacancyStrategy.cs
+++ src/SFA.Apprenticeships.Application.Candidate/Strategies/Apprenticeships/DeleteSavedApprenticeshipApprenticeshipVacancyStrategy.cs
@@ -1,20 +1,24 @@
 ﻿namespace SFA.Apprenticeships.Application.Candidate.Strategies.Apprenticeships
 {
     using System;
+    using Application.Entities;
     using Domain.Entities.Applications;
+    using Domain.Interfaces.Messaging;
     using Domain.Interfaces.Repositories;
 
     public class DeleteSavedApprenticeshipApprenticeshipVacancyStrategy : IDeleteSavedApprenticeshipVacancyStrategy
     {
         private readonly IApprenticeshipApplicationReadRepository _apprenticeshipApplicationReadRepository;
         private readonly IApprenticeshipApplicationWriteRepository _apprenticeshipApplicationWriteRepository;
+        private readonly IServiceBus _serviceBus;
 
         public DeleteSavedApprenticeshipApprenticeshipVacancyStrategy(
             IApprenticeshipApplicationReadRepository apprenticeshipApplicationReadRepository,
-            IApprenticeshipApplicationWriteRepository apprenticeshipApplicationWriteRepository)
+            IApprenticeshipApplicationWriteRepository apprenticeshipApplicationWriteRepository, IServiceBus serviceBus)
         {
             _apprenticeshipApplicationReadRepository = apprenticeshipApplicationReadRepository;
             _apprenticeshipApplicationWriteRepository = apprenticeshipApplicationWriteRepository;
+            _serviceBus = serviceBus;
         }
 
         public ApprenticeshipApplicationDetail DeleteSavedVacancy(Guid candidateId, int vacancyId)
@@ -27,6 +31,7 @@
 
             // Only actually delete a saved vacancy.
             _apprenticeshipApplicationWriteRepository.Delete(applicationDetail.EntityId);
+            _serviceBus.PublishMessage(new ApprenticeshipApplicationUpdate(applicationDetail.EntityId, ApplicationUpdateType.Delete));
             return null;
         }
     }
