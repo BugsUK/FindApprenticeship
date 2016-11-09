41aa2bdec4dfbd81e7e42f07a1fafafc3fc607b7
David Winchurch <david.winchurch@iterativeit.com>
2016-11-09 11:34:39 +0000

    RA-501: Adding AnalyticsConfiguration

---
+++ src/SFA.Apprenticeships.Web.Common/Configuration/AnalyticsConfiguration.cs
@@ -0,0 +1,12 @@
+﻿namespace SFA.Apprenticeships.Web.Common.Configuration
+{
+    public class AnalyticsConfiguration
+    {
+        public bool EnableWebTrends { get; set; }
+        public string WebTrendsDscId { get; set; }
+        public bool EnableGoogleTagManager { get; set; }
+        public string GoogleContainerId { get; set; }
+        public bool EnableAppInsights { get; set; }
+        public string AppInsightsInstrumentationKey { get; set; }
+    }
+}
