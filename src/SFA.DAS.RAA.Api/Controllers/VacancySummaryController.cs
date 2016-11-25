a23522cb7f6b1f6bc7ece4ce17c288b562921ae5
David Winchurch <david.winchurch@iterativeit.com>
2016-11-25 16:17:25 +0000

    Added test to confirm. Turned on warnings as errors

---
+++ src/SFA.DAS.RAA.Api/Controllers/VacancySummaryController.cs
@@ -0,0 +1,15 @@
+﻿namespace SFA.DAS.RAA.Api.Controllers
+{
+    using System.Collections.Generic;
+    using System.Web.Http;
+    using Swashbuckle.Swagger.Annotations;
+
+    public class VacancySummaryController : ApiController
+    {
+        [SwaggerOperation("GetAll")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
