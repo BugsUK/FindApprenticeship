a702919256355e789fe14289948bbb8b21ee3fa1
David Winchurch <david.winchurch@iterativeit.com>
2016-11-29 10:33:35 +0000

    Need IoC

--- src/SFA.DAS.RAA.Api/Handlers/ApiKeyHandler.cs
+++ src/SFA.DAS.RAA.Api/Handlers/ApiKeyHandler.cs
@@ -1,38 +1,31 @@
 ﻿namespace SFA.DAS.RAA.Api.Handlers
 {
     using System.Net.Http;
-    using System.Security.Claims;
     using System.Threading;
     using System.Threading.Tasks;
     using Extensions;
+    using Services;
 
     public class ApiKeyHandler : DelegatingHandler
     {
-        //set a default API key 
-        private const string ApiKeyQueryStringParam = "api_key";
+        private readonly IAuthenticationService _authenticationService;
+
+        public ApiKeyHandler(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
 
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            string apiKeyValue;
-            
-            //Validate that the api key exists
-            if (request.TryGetQueryStringValue(ApiKeyQueryStringParam, out apiKeyValue))
-            {
-                var userIdClaim = new Claim(ClaimTypes.SerialNumber, apiKeyValue);
-                var identity = new ClaimsIdentity(new[] { userIdClaim }, "ApiKey");
-                var principal = new ClaimsPrincipal(identity);
-
-                Thread.CurrentPrincipal = principal;
-            }
+            //Set the current principal based on authentication response
+            var principal = _authenticationService.Authenticate(request.GetQueryStrings());
+            Thread.CurrentPrincipal = principal;
 
             //Allow the request to process further down the pipeline
             var response = await base.SendAsync(request, cancellationToken);
 
             //Return the response back up the chain
             return response;
-
-            //If the key is missing, return an http status code.
-            //return request.CreateResponse(HttpStatusCode.Forbidden, "Missing API Key");
         }
     }
 }
