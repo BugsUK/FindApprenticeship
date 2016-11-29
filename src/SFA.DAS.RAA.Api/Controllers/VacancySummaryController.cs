481de9d7146450d1bbc3bdb02402a39f72c4c309
David Winchurch <david.winchurch@iterativeit.com>
2016-11-29 15:11:06 +0000

    Using ApiKeyHandler

--- src/SFA.DAS.RAA.Api/Controllers/VacancySummaryController.cs
+++ src/SFA.DAS.RAA.Api/Controllers/VacancySummaryController.cs
@@ -2,21 +2,10 @@
 {
     using System.Collections.Generic;
     using System.Web.Http;
-    using Repositories;
-    using Services;
     using Swashbuckle.Swagger.Annotations;
 
     public class VacancySummaryController : ApiController
     {
-        private readonly IRaaApiUserRepository _raaApiUserRepository;
-        private readonly IAuthenticationService _authenticationService;
-
-        public VacancySummaryController(IRaaApiUserRepository raaApiUserRepository, IAuthenticationService authenticationService)
-        {
-            _raaApiUserRepository = raaApiUserRepository;
-            _authenticationService = authenticationService;
-        }
-
         [SwaggerOperation("GetAll")]
         public IEnumerable<string> Get()
         {
--- src/SFA.DAS.RAA.Api/Global.asax.cs
+++ src/SFA.DAS.RAA.Api/Global.asax.cs
@@ -1,18 +1,24 @@
 ﻿namespace SFA.DAS.RAA.Api
 {
     using System.Web.Http;
+    using DependencyResolution;
     using Handlers;
+    using Services;
 
     public class WebApiApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
+            //Configure IoC
+            var container = IoC.Initialize();
+            GlobalConfiguration.Configuration.DependencyResolver = new StructureMapWebApiDependencyResolver(container);
+
+            //Register API Key handler
+            GlobalConfiguration.Configuration.MessageHandlers.Add(new ApiKeyHandler(container.GetInstance<IAuthenticationService>()));
+
             GlobalConfiguration.Configure(WebApiConfig.Register);
 
-            //GlobalConfiguration.Configuration.MessageHandlers.Add(new ApiKeyHandler());
-
-            Microsoft.ApplicationInsights.Extensibility.TelemetryConfiguration.Active.InstrumentationKey =
-                System.Web.Configuration.WebConfigurationManager.AppSettings["APPINSIGHTS_INSTRUMENTATIONKEY"];
+            Microsoft.ApplicationInsights.Extensibility.TelemetryConfiguration.Active.InstrumentationKey = System.Web.Configuration.WebConfigurationManager.AppSettings["APPINSIGHTS_INSTRUMENTATIONKEY"];
         }
     }
 }
