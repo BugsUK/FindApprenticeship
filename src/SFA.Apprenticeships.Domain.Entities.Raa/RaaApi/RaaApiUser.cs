9e67a19adc55e0e6595c563defd74fa533e2b64c
David Winchurch <david.winchurch@iterativeit.com>
2016-11-30 09:24:46 +0000

    Adding database entries for RaaApiUser

--- src/SFA.Apprenticeships.Domain.Entities.Raa/RaaApi/RaaApiUser.cs
+++ src/SFA.Apprenticeships.Domain.Entities.Raa/RaaApi/RaaApiUser.cs
@@ -4,12 +4,11 @@
 
     public class RaaApiUser
     {
-        public static readonly RaaApiUser UnknownApiUser = new RaaApiUser {Name = "UnknownApiUser"};
+        public static readonly RaaApiUser UnknownApiUser = new RaaApiUser();
 
-        /// <summary>
-        /// The api key that identified this user
-        /// </summary>
-        public string Name { get; set; }
+        public Guid PrimaryApiKey { get; set; }
+
+        public Guid SecondaryApiKey { get; set; }
 
         /// <summary>
         /// The user's type
@@ -19,42 +18,16 @@
         /// <summary>
         /// The entities primary key in the database
         /// </summary>
-        public int Id { get; set; }
+        public int ReferencedEntityId { get; set; }
 
         /// <summary>
         /// The entities primary identifier in the database
         /// </summary>
-        public Guid Guid { get; set; }
+        public Guid ReferencedEntityGuid { get; set; }
 
         /// <summary>
         /// Can be a UKPRN, EDSURN or other identifier
         /// </summary>
-        public string SurrogateId { get; set; }
-
-        protected bool Equals(RaaApiUser other)
-        {
-            return string.Equals(Name, other.Name) && UserType == other.UserType && Id == other.Id && Guid.Equals(other.Guid) && string.Equals(SurrogateId, other.SurrogateId);
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((RaaApiUser) obj);
-        }
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                var hashCode = (Name != null ? Name.GetHashCode() : 0);
-                hashCode = (hashCode*397) ^ (int) UserType;
-                hashCode = (hashCode*397) ^ Id;
-                hashCode = (hashCode*397) ^ Guid.GetHashCode();
-                hashCode = (hashCode*397) ^ (SurrogateId != null ? SurrogateId.GetHashCode() : 0);
-                return hashCode;
-            }
-        }
+        public int ReferencedEntitySurrogateId { get; set; }
     }
 }
