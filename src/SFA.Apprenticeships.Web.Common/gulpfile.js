9ec88fa5952b141fb7bd8ad7c65c16c996d5e7d9
adotellison <adotellison@gmail.com>
2016-12-14 16:37:18 +0000

    Add sfa-button-secondary class and replace all link-buttons with it

--- src/SFA.Apprenticeships.Web.Common/gulpfile.js
+++ src/SFA.Apprenticeships.Web.Common/gulpfile.js
@@ -10,6 +10,7 @@ var merge = require('merge-stream');
 
 var repo_root = __dirname + '/';
 var govuk_frontend_toolkit_root = repo_root + 'node_modules/govuk_frontend_toolkit/stylesheets'; // 1.
+var govuk_frontend_toolkit_design_patterns = repo_root + 'node_modules/govuk_frontend_toolkit/stylesheets/design-patterns'; // 1.
 var govuk_elements_sass_root = repo_root + 'node_modules/govuk-elements-sass/public/sass';       // 2.
 
 var buildDir = './build/';  
@@ -34,7 +35,8 @@ gulp.task('styles', ['clean', 'cleantarget'], function () {
       .pipe(sass({
           includePaths: [
             govuk_frontend_toolkit_root,
-            govuk_elements_sass_root
+            govuk_elements_sass_root,
+            govuk_frontend_toolkit_design_patterns
           ]
       }).on('error', sass.logError))
       .pipe(gulp.dest(buildDir + 'css'));
