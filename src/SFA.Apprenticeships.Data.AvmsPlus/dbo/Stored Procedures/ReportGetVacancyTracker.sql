e3b2cd2d4101e5aa655dfa4f77a0f1aefd5b225b
Shoma Gujjar <shoma.gujjar@valtech.co.uk>
2016-12-15 11:27:33 +0000

    ra-560: add new stored proc to retrive vacancy tracker info.

---
+++ src/SFA.Apprenticeships.Data.AvmsPlus/dbo/Stored Procedures/ReportGetVacancyTracker.sql
@@ -0,0 +1,17 @@
+﻿/*----------------------------------------------------------------------                  
+Name  : ReportGetVacancyTracker                 
+Description :  Returns Actioned Vacancy by Serco between the given dates.
+                
+History:                  
+--------                  
+Date			Version		Author			Comment
+26-Aug-2008		1.0			Femma Ashraf	first version
+05-Oct-2008		1.1			Ian Emery		removed parameter @type
+---------------------------------------------------------------------- */ 
+
+CREATE PROCEDURE [dbo].[GetVacancyTracker]
+	@param1 int = 0,
+	@param2 int
+AS
+	SELECT @param1, @param2
+RETURN 0
