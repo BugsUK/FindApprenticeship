ebed50be02d676275a0139befe7176f13742d43e
David Winchurch <david.winchurch@iterativeit.com>
2016-12-15 08:12:33 +0000

    RA-388: Unit test to confirm that wage is updated

---
+++ src/SFA.DAS.RAA.Api.UnitTests/Controllers/VacancyManagementControllerTests.cs
@@ -0,0 +1,26 @@
+﻿namespace SFA.DAS.RAA.Api.UnitTests.Controllers
+{
+    using Api.Controllers;
+    using Apprenticeships.Domain.Entities.Raa.Vacancies;
+    using Moq;
+    using NUnit.Framework;
+    using Ploeh.AutoFixture;
+    using Providers;
+
+    [TestFixture]
+    [Parallelizable]
+    public class VacancyManagementControllerTests
+    {
+        [Test]
+        public void EditWageUpdatesVacancy()
+        {
+            var vacancy = new Fixture().Build<Vacancy>().Create();
+            var vacancyProvider = new Mock<IVacancyProvider>();
+            vacancyProvider.Setup(p => p.Get(1, null, null)).Returns(vacancy);
+
+            var controller = new VacancyManagementController(vacancyProvider.Object);
+
+
+        }
+    }
+}
