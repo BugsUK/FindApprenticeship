c285fc72dcd7edadc65d4523ed685ae62503ca76
David Winchurch <david.winchurch@iterativeit.com>
2016-12-16 11:00:40 +0000

    RA-388: Implemented all wage type change validation

--- src/SFA.DAS.RAA.Api.UnitTests/Validators/WageUpdateValidatorTests.cs
+++ src/SFA.DAS.RAA.Api.UnitTests/Validators/WageUpdateValidatorTests.cs
@@ -16,6 +16,154 @@
         [TestCase(WageType.LegacyWeekly, false)]
         [TestCase(WageType.ApprenticeshipMinimum, false)]
         [TestCase(WageType.NationalMinimum, false)]
+        [TestCase(WageType.Custom, false)]
+        [TestCase(WageType.CustomRange, false)]
+        [TestCase(WageType.CompetitiveSalary, false)]
+        [TestCase(WageType.ToBeAgreedUponAppointment, false)]
+        [TestCase(WageType.Unwaged, false)]
+        public void ValidLegacyTextWageTypeChanges(WageType newWageType, bool expectedIsValid)
+        {
+            var wageUpdate = new WageUpdate
+            {
+                Type = newWageType,
+                Amount = 110,
+                AmountLowerBound = 110,
+                AmountUpperBound = 120,
+                Unit = WageUnit.Weekly,
+                ExistingType = WageType.LegacyText,
+                ExistingAmount = 100
+            };
+
+            var validator = new WageUpdateValidator();
+
+            var validationResult = validator.Validate(wageUpdate, ruleSet: WageUpdateValidator.CompareWithExisting);
+
+            validationResult.IsValid.Should().Be(expectedIsValid);
+            if (expectedIsValid)
+            {
+                validator.ShouldNotHaveValidationErrorFor(wu => wu.Type, wageUpdate, WageUpdateValidator.CompareWithExisting);
+            }
+            else
+            {
+                validator.ShouldHaveValidationErrorFor(wu => wu.Type, wageUpdate, WageUpdateValidator.CompareWithExisting).WithErrorMessage("You cannot change the type of a LegacyText wage.");
+            }
+        }
+
+        [TestCase(WageType.LegacyText, false)]
+        [TestCase(WageType.LegacyWeekly, true)]
+        [TestCase(WageType.ApprenticeshipMinimum, false)]
+        [TestCase(WageType.NationalMinimum, false)]
+        [TestCase(WageType.Custom, true)]
+        [TestCase(WageType.CustomRange, true)]
+        [TestCase(WageType.CompetitiveSalary, false)]
+        [TestCase(WageType.ToBeAgreedUponAppointment, false)]
+        [TestCase(WageType.Unwaged, false)]
+        public void ValidLegacyWeeklyWageTypeChanges(WageType newWageType, bool expectedIsValid)
+        {
+            var wageUpdate = new WageUpdate
+            {
+                Type = newWageType,
+                Amount = 110,
+                AmountLowerBound = 110,
+                AmountUpperBound = 120,
+                Unit = WageUnit.Weekly,
+                ExistingType = WageType.LegacyWeekly,
+                ExistingAmount = 100
+            };
+
+            var validator = new WageUpdateValidator();
+
+            var validationResult = validator.Validate(wageUpdate, ruleSet: WageUpdateValidator.CompareWithExisting);
+
+            validationResult.IsValid.Should().Be(expectedIsValid);
+            if (expectedIsValid)
+            {
+                validator.ShouldNotHaveValidationErrorFor(wu => wu.Type, wageUpdate, WageUpdateValidator.CompareWithExisting);
+            }
+            else
+            {
+                validator.ShouldHaveValidationErrorFor(wu => wu.Type, wageUpdate, WageUpdateValidator.CompareWithExisting).WithErrorMessage("You can only change the type of a LegacyWeekly wage to Custom (fixed) or CustomRange (wage range).");
+            }
+        }
+
+        [TestCase(WageType.LegacyText, false)]
+        [TestCase(WageType.LegacyWeekly, false)]
+        [TestCase(WageType.ApprenticeshipMinimum, true)]
+        [TestCase(WageType.NationalMinimum, true)]
+        [TestCase(WageType.Custom, true)]
+        [TestCase(WageType.CustomRange, true)]
+        [TestCase(WageType.CompetitiveSalary, false)]
+        [TestCase(WageType.ToBeAgreedUponAppointment, false)]
+        [TestCase(WageType.Unwaged, false)]
+        public void ValidApprenticeshipMinimumWageTypeChanges(WageType newWageType, bool expectedIsValid)
+        {
+            var wageUpdate = new WageUpdate
+            {
+                Type = newWageType,
+                Amount = 110,
+                AmountLowerBound = 110,
+                AmountUpperBound = 120,
+                Unit = WageUnit.Weekly,
+                ExistingType = WageType.ApprenticeshipMinimum,
+                ExistingAmount = 100
+            };
+
+            var validator = new WageUpdateValidator();
+
+            var validationResult = validator.Validate(wageUpdate, ruleSet: WageUpdateValidator.CompareWithExisting);
+
+            validationResult.IsValid.Should().Be(expectedIsValid);
+            if (expectedIsValid)
+            {
+                validator.ShouldNotHaveValidationErrorFor(wu => wu.Type, wageUpdate, WageUpdateValidator.CompareWithExisting);
+            }
+            else
+            {
+                validator.ShouldHaveValidationErrorFor(wu => wu.Type, wageUpdate, WageUpdateValidator.CompareWithExisting).WithErrorMessage("You can only change the type of an ApprenticeshipMinimum wage to NationalMinimum, Custom (fixed) or CustomRange (wage range).");
+            }
+        }
+
+        [TestCase(WageType.LegacyText, false)]
+        [TestCase(WageType.LegacyWeekly, false)]
+        [TestCase(WageType.ApprenticeshipMinimum, false)]
+        [TestCase(WageType.NationalMinimum, true)]
+        [TestCase(WageType.Custom, true)]
+        [TestCase(WageType.CustomRange, true)]
+        [TestCase(WageType.CompetitiveSalary, false)]
+        [TestCase(WageType.ToBeAgreedUponAppointment, false)]
+        [TestCase(WageType.Unwaged, false)]
+        public void ValidNationalMinimumWageTypeChanges(WageType newWageType, bool expectedIsValid)
+        {
+            var wageUpdate = new WageUpdate
+            {
+                Type = newWageType,
+                Amount = 110,
+                AmountLowerBound = 110,
+                AmountUpperBound = 120,
+                Unit = WageUnit.Weekly,
+                ExistingType = WageType.NationalMinimum,
+                ExistingAmount = 100
+            };
+
+            var validator = new WageUpdateValidator();
+
+            var validationResult = validator.Validate(wageUpdate, ruleSet: WageUpdateValidator.CompareWithExisting);
+
+            validationResult.IsValid.Should().Be(expectedIsValid);
+            if (expectedIsValid)
+            {
+                validator.ShouldNotHaveValidationErrorFor(wu => wu.Type, wageUpdate, WageUpdateValidator.CompareWithExisting);
+            }
+            else
+            {
+                validator.ShouldHaveValidationErrorFor(wu => wu.Type, wageUpdate, WageUpdateValidator.CompareWithExisting).WithErrorMessage("You can only change the type of a NationalMinimum wage to Custom (fixed) or CustomRange (wage range).");
+            }
+        }
+
+        [TestCase(WageType.LegacyText, false)]
+        [TestCase(WageType.LegacyWeekly, false)]
+        [TestCase(WageType.ApprenticeshipMinimum, false)]
+        [TestCase(WageType.NationalMinimum, false)]
         [TestCase(WageType.Custom, true)]
         [TestCase(WageType.CustomRange, true)]
         [TestCase(WageType.CompetitiveSalary, false)]
@@ -31,7 +179,7 @@
                 AmountUpperBound = 120,
                 Unit = WageUnit.Weekly,
                 ExistingType = WageType.Custom,
-                ExistingAmount = 100,
+                ExistingAmount = 100
             };
 
             var validator = new WageUpdateValidator();
@@ -68,7 +216,7 @@
                 AmountUpperBound = 120,
                 Unit = WageUnit.Weekly,
                 ExistingType = WageType.CustomRange,
-                ExistingAmount = 100,
+                ExistingAmount = 100
             };
 
             var validator = new WageUpdateValidator();
@@ -83,6 +231,117 @@
             else
             {
                 validator.ShouldHaveValidationErrorFor(wu => wu.Type, wageUpdate, WageUpdateValidator.CompareWithExisting).WithErrorMessage("You can only change the type of a CustomRange (wage range) wage to Custom (fixed).");
+            }
+        }
+
+        [TestCase(WageType.LegacyText, false)]
+        [TestCase(WageType.LegacyWeekly, false)]
+        [TestCase(WageType.ApprenticeshipMinimum, false)]
+        [TestCase(WageType.NationalMinimum, false)]
+        [TestCase(WageType.Custom, false)]
+        [TestCase(WageType.CustomRange, false)]
+        [TestCase(WageType.CompetitiveSalary, false)]
+        [TestCase(WageType.ToBeAgreedUponAppointment, false)]
+        [TestCase(WageType.Unwaged, false)]
+        public void ValidCompetitiveSalaryWageTypeChanges(WageType newWageType, bool expectedIsValid)
+        {
+            var wageUpdate = new WageUpdate
+            {
+                Type = newWageType,
+                Amount = 110,
+                AmountLowerBound = 110,
+                AmountUpperBound = 120,
+                Unit = WageUnit.Weekly,
+                ExistingType = WageType.CompetitiveSalary,
+                ExistingAmount = 100
+            };
+
+            var validator = new WageUpdateValidator();
+
+            var validationResult = validator.Validate(wageUpdate, ruleSet: WageUpdateValidator.CompareWithExisting);
+
+            validationResult.IsValid.Should().Be(expectedIsValid);
+            if (expectedIsValid)
+            {
+                validator.ShouldNotHaveValidationErrorFor(wu => wu.Type, wageUpdate, WageUpdateValidator.CompareWithExisting);
+            }
+            else
+            {
+                validator.ShouldHaveValidationErrorFor(wu => wu.Type, wageUpdate, WageUpdateValidator.CompareWithExisting).WithErrorMessage("You cannot change the type of a CompetitiveSalary wage.");
+            }
+        }
+
+        [TestCase(WageType.LegacyText, false)]
+        [TestCase(WageType.LegacyWeekly, false)]
+        [TestCase(WageType.ApprenticeshipMinimum, false)]
+        [TestCase(WageType.NationalMinimum, false)]
+        [TestCase(WageType.Custom, false)]
+        [TestCase(WageType.CustomRange, false)]
+        [TestCase(WageType.CompetitiveSalary, false)]
+        [TestCase(WageType.ToBeAgreedUponAppointment, false)]
+        [TestCase(WageType.Unwaged, false)]
+        public void ValidToBeAgreedUponAppointmentWageTypeChanges(WageType newWageType, bool expectedIsValid)
+        {
+            var wageUpdate = new WageUpdate
+            {
+                Type = newWageType,
+                Amount = 110,
+                AmountLowerBound = 110,
+                AmountUpperBound = 120,
+                Unit = WageUnit.Weekly,
+                ExistingType = WageType.ToBeAgreedUponAppointment,
+                ExistingAmount = 100
+            };
+
+            var validator = new WageUpdateValidator();
+
+            var validationResult = validator.Validate(wageUpdate, ruleSet: WageUpdateValidator.CompareWithExisting);
+
+            validationResult.IsValid.Should().Be(expectedIsValid);
+            if (expectedIsValid)
+            {
+                validator.ShouldNotHaveValidationErrorFor(wu => wu.Type, wageUpdate, WageUpdateValidator.CompareWithExisting);
+            }
+            else
+            {
+                validator.ShouldHaveValidationErrorFor(wu => wu.Type, wageUpdate, WageUpdateValidator.CompareWithExisting).WithErrorMessage("You cannot change the type of a ToBeAgreedUponAppointment wage.");
+            }
+        }
+
+        [TestCase(WageType.LegacyText, false)]
+        [TestCase(WageType.LegacyWeekly, false)]
+        [TestCase(WageType.ApprenticeshipMinimum, true)]
+        [TestCase(WageType.NationalMinimum, true)]
+        [TestCase(WageType.Custom, true)]
+        [TestCase(WageType.CustomRange, true)]
+        [TestCase(WageType.CompetitiveSalary, false)]
+        [TestCase(WageType.ToBeAgreedUponAppointment, false)]
+        [TestCase(WageType.Unwaged, true)]
+        public void ValidUnwagedWageTypeChanges(WageType newWageType, bool expectedIsValid)
+        {
+            var wageUpdate = new WageUpdate
+            {
+                Type = newWageType,
+                Amount = 110,
+                AmountLowerBound = 110,
+                AmountUpperBound = 120,
+                Unit = WageUnit.Weekly,
+                ExistingType = WageType.Unwaged,
+                ExistingAmount = 100
+            };
+
+            var validator = new WageUpdateValidator();
+
+            var validationResult = validator.Validate(wageUpdate, ruleSet: WageUpdateValidator.CompareWithExisting);
+
+            validationResult.IsValid.Should().Be(expectedIsValid);
+            if (expectedIsValid)
+            {
+                validator.ShouldNotHaveValidationErrorFor(wu => wu.Type, wageUpdate, WageUpdateValidator.CompareWithExisting);
+            }
+            else
+            {
+                validator.ShouldHaveValidationErrorFor(wu => wu.Type, wageUpdate, WageUpdateValidator.CompareWithExisting).WithErrorMessage("You can only change the type of an Unwaged wage to ApprenticeshipMinimum, NationalMinimum, Custom (fixed) or CustomRange (wage range).");
             }
         }
     }
--- src/SFA.DAS.RAA.Api/Validators/WageUpdateValidator.cs
+++ src/SFA.DAS.RAA.Api/Validators/WageUpdateValidator.cs
@@ -33,6 +33,35 @@
             }
 
             const string propertyName = "Type";
+            if (wageUpdate.ExistingType == WageType.LegacyText)
+            {
+                return new ValidationFailure(propertyName, "You cannot change the type of a LegacyText wage.");
+            }
+
+            if (wageUpdate.ExistingType == WageType.LegacyWeekly)
+            {
+                if (wageUpdate.Type != WageType.LegacyWeekly && wageUpdate.Type != WageType.Custom && wageUpdate.Type != WageType.CustomRange)
+                {
+                    return new ValidationFailure(propertyName, "You can only change the type of a LegacyWeekly wage to Custom (fixed) or CustomRange (wage range).");
+                }
+            }
+
+            if (wageUpdate.ExistingType == WageType.ApprenticeshipMinimum)
+            {
+                if (wageUpdate.Type != WageType.ApprenticeshipMinimum && wageUpdate.Type != WageType.NationalMinimum && wageUpdate.Type != WageType.Custom && wageUpdate.Type != WageType.CustomRange)
+                {
+                    return new ValidationFailure(propertyName, "You can only change the type of an ApprenticeshipMinimum wage to NationalMinimum, Custom (fixed) or CustomRange (wage range).");
+                }
+            }
+
+            if (wageUpdate.ExistingType == WageType.NationalMinimum)
+            {
+                if (wageUpdate.Type != WageType.NationalMinimum && wageUpdate.Type != WageType.Custom && wageUpdate.Type != WageType.CustomRange)
+                {
+                    return new ValidationFailure(propertyName, "You can only change the type of a NationalMinimum wage to Custom (fixed) or CustomRange (wage range).");
+                }
+            }
+
             if (wageUpdate.ExistingType == WageType.Custom || wageUpdate.ExistingType == WageType.CustomRange)
             {
                 if (wageUpdate.Type != WageType.Custom && wageUpdate.Type != WageType.CustomRange)
@@ -41,6 +70,24 @@
                         wageUpdate.ExistingType == WageType.Custom
                             ? "You can only change the type of a Custom (fixed) wage to CustomRange (wage range)."
                             : "You can only change the type of a CustomRange (wage range) wage to Custom (fixed).");
+                }
+            }
+
+            if (wageUpdate.ExistingType == WageType.CompetitiveSalary)
+            {
+                return new ValidationFailure(propertyName, "You cannot change the type of a CompetitiveSalary wage.");
+            }
+
+            if (wageUpdate.ExistingType == WageType.ToBeAgreedUponAppointment)
+            {
+                return new ValidationFailure(propertyName, "You cannot change the type of a ToBeAgreedUponAppointment wage.");
+            }
+
+            if (wageUpdate.ExistingType == WageType.Unwaged)
+            {
+                if (wageUpdate.Type != WageType.Unwaged && wageUpdate.Type != WageType.ApprenticeshipMinimum && wageUpdate.Type != WageType.NationalMinimum && wageUpdate.Type != WageType.Custom && wageUpdate.Type != WageType.CustomRange)
+                {
+                    return new ValidationFailure(propertyName, "You can only change the type of an Unwaged wage to ApprenticeshipMinimum, NationalMinimum, Custom (fixed) or CustomRange (wage range).");
                 }
             }
 
