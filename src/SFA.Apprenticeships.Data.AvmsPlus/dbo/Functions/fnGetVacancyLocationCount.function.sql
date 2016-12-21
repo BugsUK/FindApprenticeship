94a89827913b1e83f1ae4f0b4f45fb97adf185b9
Aaron Rhodes <aaroncraigrhodes@googlemail.com>
2016-12-21 15:28:54 +0000

    Adding sql function to project

---
+++ src/SFA.Apprenticeships.Data.AvmsPlus/dbo/Functions/fnGetVacancyLocationCount.function.sql
@@ -0,0 +1,20 @@
+﻿/*----------------------------------------------------------------------                               
+Name		:	dbo.GetVacancyLocationCount
+Description :	returns the number of addresses added for the given vacancy
+                
+History:                  
+--------                  
+Date			Version		Author			Comment
+21-12-2016		1.0			Aaron Rhodes	first version
+
+---------------------------------------------------------------------- */                 
+
+CREATE FUNCTION [dbo].[GetVacancyLocationCount] (@vacancyId INT)
+RETURNS INT
+AS BEGIN
+	DECLARE @count INT
+	SELECT @count = COUNT(*) FROM VacancyLocation WHERE VacancyId = @vacancyId
+	
+	RETURN @count
+END
+GO
