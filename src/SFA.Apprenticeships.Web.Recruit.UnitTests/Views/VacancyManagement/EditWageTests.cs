abc2fdca7f29c239ab70e82c6f85ed1fd53032db
David Winchurch <david.winchurch@iterativeit.com>
2017-01-04 10:35:59 +0000

    RA-388: Adding view unit tests for editing wage

---
+++ src/SFA.Apprenticeships.Web.Recruit.UnitTests/Views/VacancyManagement/EditWageTests.cs
@@ -0,0 +1,20 @@
+﻿namespace SFA.Apprenticeships.Web.Recruit.UnitTests.Views.VacancyManagement
+{
+    using Builders;
+    using Domain.Entities.Vacancies;
+    using NUnit.Framework;
+    using RazorGenerator.Testing;
+    using Recruit.Views.VacancyManagement;
+
+    [TestFixture]
+    public class EditWageTests
+    {
+        [Test]
+        public void EditFixedWageChoices()
+        {
+            var viewModel = new EditWageViewModelBuilder(WageType.Custom).Build();
+
+            var view = new EditWage().RenderAsHtml(viewModel);
+        }
+    }
+}
