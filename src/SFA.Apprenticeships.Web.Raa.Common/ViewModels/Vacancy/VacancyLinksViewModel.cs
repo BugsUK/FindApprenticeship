a6cb9530f5a139402c206aff413af24ac88714d8
David Winchurch <david.winchurch@iterativeit.com>
2017-01-06 10:49:50 +0000

    RA-388: Ensure you can only increase the wage of valid, non legacy vacancies

--- src/SFA.Apprenticeships.Web.Raa.Common/ViewModels/Vacancy/VacancyLinksViewModel.cs
+++ src/SFA.Apprenticeships.Web.Raa.Common/ViewModels/Vacancy/VacancyLinksViewModel.cs
@@ -1,19 +1,21 @@
 ﻿namespace SFA.Apprenticeships.Web.Raa.Common.ViewModels.Vacancy
 {
     using Domain.Entities.Raa.Vacancies;
+    using Web.Common.ViewModels;
 
     public class VacancyLinksViewModel
     {
         public const string PartialView = "DisplayTemplates/_VacancyLinks";
 
         public VacancyLinksViewModel(int vacancyReferenceNumber,
-            VacancyStatus status, int totalNumberOfApplications, VacancyType vacancyType, int totalNumberOfApplicationsWithPendingDecision)
+            VacancyStatus status, int totalNumberOfApplications, VacancyType vacancyType, int totalNumberOfApplicationsWithPendingDecision, WageViewModel wage)
         {
             VacancyReferenceNumber = vacancyReferenceNumber;
             Status = status;
             TotalNumberOfApplications = totalNumberOfApplications;
             VacancyType = vacancyType;
             TotalNumberOfApplicationsWithPendingDecision = totalNumberOfApplicationsWithPendingDecision;
+            Wage = wage;
         }
 
         public int VacancyReferenceNumber { get; private set; }
@@ -21,5 +23,6 @@
         public int TotalNumberOfApplications { get; private set; }
         public VacancyType VacancyType { get; private set; }
         public int TotalNumberOfApplicationsWithPendingDecision { get; private set; }
+        public WageViewModel Wage { get; private set; }
     }
 }
